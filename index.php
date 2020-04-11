f8144c388b06428715730c85e6fd78d964bcadda
akshay / jigmet <akshay.wildfire@pm.me>
2020-04-12 02:35:51 +0530

    main index.php to handle theme loading for front end url

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 include_once ('config-init.php');
-include_once ('header.php');
-?>
-
-
-<?php include_once ('footer.php'); ?>
+$ext=explode('/', $_GET['ext']);
+if (!$ext[0])	include_once (THEME_PATH.'/index.php');
+elseif (($type=$ext[0]) && ($slug=$ext[1])) {include_once (THEME_PATH.'/single.php');}
+elseif ($type) {include_once (THEME_PATH.'/archive.php');}
+else {include_once (THEME_PATH.'/404.php');}
