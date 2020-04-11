d535a9493e4c4093b0e4879506f3fb4cc7e5ab7f
akshay / jigmet <akshay.wildfire@pm.me>
2020-04-12 02:34:03 +0530

    new theme folder

---
+++ themes/2020/footer.php
@@ -0,0 +1,16 @@
+
+	<script src="/plugins/jquery.min.js"></script>
+	<script src="/plugins/popper/popper.min.js"></script>
+	<script src="/plugins/moment.js"></script>
+	<script src="/plugins/bootstrap/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="/plugins/typeout/typeout.js"></script>
+	<script src="/plugins/datatables/datatables.min.js"></script>
+	<script src="/plugins/clipboard.min.js"></script>
+	<script src="<?php echo THEME_URL; ?>/js/custom.js?v=<?php echo time(); ?>"></script>
+
+	<script src="https://blueimp.github.io/jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
+	<script src="https://blueimp.github.io/jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
+	<script src="https://blueimp.github.io/jQuery-File-Upload/js/jquery.fileupload.js"></script>
+    <?php if (GOOGLE_MAP_API_KEY_1)	echo '<script src="https://maps.googleapis.com/maps/api/js?key='.GOOGLE_MAP_API_KEY_1.'&libraries=places&callback=initMap" async defer></script>'; ?>
+</body>
+</html>
