f7d938db352faab43f063c813588124c4a31656a
akshay / jigmet <akshay.wildfire@pm.me>
2020-04-14 14:57:50 +0530

    json to array, all types and menus variables changed across

--- includes/theme.class.php
+++ includes/theme.class.php
@@ -32,20 +32,20 @@ class theme {
 		if ($items) {
 			$op.='
 			<nav class="navbar navbar-expand-lg navbar-light bg-light">
-				<a class="navbar-brand" href="'.$items->logo->href.'" title="'.$items->logo->title.'">'.($items->logo->src?'<img height="'.$items->logo->height.'" src="'.$items->logo->src.'">':$items->logo->name).'</a>
+				<a class="navbar-brand" href="'.$items['logo']['href'].'" title="'.$items['logo']['title'].'">'.($items['logo']['src']?'<img height="'.$items['logo']['height'].'" src="'.$items['logo']['src'].'">':$items['logo']['name']).'</a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
 					<span class="navbar-toggler-icon"></span>
 				</button>
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="navbar-nav ml-auto mr-0">';			
 					foreach ($items->menu as $item) {
-						if (empty($item->submenu))
-							$op.='<li class="nav-item active"><a class="nav-link" href="'.$item->href.'" title="'.$item->title.'">'.$item->name.'</a></li>';
+						if (empty($item['submenu']))
+							$op.='<li class="nav-item active"><a class="nav-link" href="'.$item->href.'" title="'.$item['title'].'">'.$item['name'].'</a></li>';
 						else {
-							$op.='<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" title="'.$item->title.'" role="button" data-toggle="dropdown">'.$item->name.'
+							$op.='<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" title="'.$item['title'].'" role="button" data-toggle="dropdown">'.$item['name'].'
 								</a><div class="dropdown-menu dropdown-menu-right">';
-							foreach ($item->submenu as $subitem)
-								$op.='<a class="dropdown-item" href="'.$subitem->href.'" title="'.($subitem->title?$subitem->title:'').'">'.$subitem->name.'</a>';
+							foreach ($item['submenu'] as $subitem)
+								$op.='<a class="dropdown-item" href="'.$subitem['href'].'" title="'.($subitem['title']?$subitem['title']:'').'">'.$subitem['name'].'</a>';
 							$op.='</div></li>';
 						}
 					}
