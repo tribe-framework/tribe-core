f8ce2af8b90695d8e179a722d54eb153fc2cf9f4
akshay / jigmet <akshay.wildfire@pm.me>
2020-04-17 22:20:10 +0530

    multi_url

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -22,7 +22,7 @@ $( document ).ready(function() {
 	});
 
 	$(document).on('click', '.multi_add_btn', function(e) {
-		$(this).closest('".'+$(this).data('group-class')+'"').first().clone().appendTo('".'+$(this).data('group-class')+'"');
+		$(this).closest('.url-group').first().clone().appendTo('.url-group');
 	});
 
 	$('.datatable').DataTable({"order": [[ 0, "desc" ]]});
