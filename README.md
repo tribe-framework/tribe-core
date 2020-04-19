28755471f61f59e5e4b30e0ac09c74f9401fc342
akshay / jigmet <akshay.wildfire@pm.me>
2020-04-19 19:11:16 +0530

    backing up mysql database to s3

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ if you're not using git to update - config, uploads and themes folders need not 
 1. sudo apt-get install s3cmd
 2. create a s3 folder, dis-allow versioning, allow logging in same directory, go to folder settings - complaince settings - set complaince mode ON, keep detele retention 3 months
 3. create a user and change user home path to folder name
-4. save user access and secret key details config/config-vars.php
+4. save s3 user access and secret key details config/config-vars.php
 
 ## contact
 write to tech@wildfire.world if you face any issues or have suggestions 
--- config/backup.php
+++ config/backup.php
@@ -1,10 +1,13 @@
 <?php
 include_once('config-vars.php');
-$backupfile='backup-'.DB_NAME.'-'.date('YmdHis').'.sql.gz';
-linux_command('mysqldump -u'.DB_USER.' -p'.DB_PASS.' '.DB_NAME.' | gzip > /var/tmp/'.$backupfile);
-$folders_to_backup=array(ABSOLUTE_PATH.'/uploads', ABSOLUTE_PATH.'/themes', '/var/tmp/'.$backupfile);
+
+$folders_to_backup=array(ABSOLUTE_PATH.'/uploads', ABSOLUTE_PATH.'/themes');
 foreach ($folders_to_backup as $folder)
 	linux_command('s3cmd sync -r --delete-removed --host="'.S3_BKUP_HOST_BASE.'" --access_key="'.S3_BKUP_ACCESS_KEY.'" --secret_key="'.S3_BKUP_SECRET_KEY.'" --host-bucket="'.S3_BKUP_HOST_BUCKET.'" '.$folder.' s3://'.S3_BKUP_FOLDER_NAME.'/');
+
+$backupfile='backup-'.DB_NAME.'.sql.gz';
+linux_command('mysqldump -u'.DB_USER.' -p'.DB_PASS.' '.DB_NAME.' | gzip > /var/tmp/'.$backupfile);
+linux_command('s3cmd --host="'.S3_BKUP_HOST_BASE.'" --access_key="'.S3_BKUP_ACCESS_KEY.'" --secret_key="'.S3_BKUP_SECRET_KEY.'" --host-bucket="'.S3_BKUP_HOST_BUCKET.'" -P put /var/tmp/'.$backupfile.' s3://'.S3_BKUP_FOLDER_NAME.'/');
 linux_command('rm /var/tmp/'.$backupfile);
 
 function linux_command ($cmd) {
