e5dc414e98a5d454b05c3f8a19a2c58bbc07d679
akshay / jigmet <akshay.wildfire@pm.me>
2020-04-25 01:42:18 +0530

    head meta data in wildfire theme

--- themes/wildfire-2020/header.php
+++ themes/wildfire-2020/header.php
@@ -3,8 +3,12 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
 	<title><?php echo (isset($postdata_modified[$headmeta_title]) ? $postdata_modified[$headmeta_title] : $types['webapp']['headmeta_title']); ?></title>
 	<meta name="description" content="<?php echo (isset($postdata_modified[$headmeta_description]) ? $postdata_modified[$headmeta_description] : $types['webapp']['headmeta_description']); ?>">
+	<meta property="og:title" content="<?php echo (isset($postdata_modified[$headmeta_title]) ? $postdata_modified[$headmeta_title] : $types['webapp']['headmeta_title']); ?>">
+	<meta property="og:description" content="<?php echo (isset($postdata_modified[$headmeta_description]) ? $postdata_modified[$headmeta_description] : $types['webapp']['headmeta_description']); ?>">
+	<meta property="og:image" content="<?php echo ($postdata['cover_media'][0]?$postdata['cover_media'][0]:$postdata['cover_media']); ?>">
 
 	<link href="/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 	<link href="/plugins/fontawesome/css/all.min.css" rel="stylesheet">
