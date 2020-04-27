773a8f4961ff1f87d070a7770db7e402a31fed80
akshay / jigmet <akshay.wildfire@pm.me>
2020-04-27 13:44:23 +0530

    midway archive changes for list.php

--- admin/list.php
+++ admin/list.php
@@ -17,9 +17,10 @@ include_once (ABSOLUTE_PATH.'/admin/header.php');
       $i=0;
       $displayed_field_slugs=array();
       foreach ($types[$type]['modules'] as $module) {
-        if (!in_array($module['input_slug'], $displayed_field_slugs))
+        if (!in_array($module['input_slug'], $displayed_field_slugs)) {
           echo (isset($module['list_field'])?'<th scope="col" class="pl-2" data-orderable="'.(isset($module['list_sortable'])?'true':'false').'" data-searchable="'.(isset($module['list_searchable'])?'true':'false').'" '.(isset($module['input_primary'])?'style="max-width:50%"':'').'>'.$module['input_slug'].'</th>':'');
-        $displayed_field_slugs[]=$module['input_slug'];
+          $displayed_field_slugs[]=$module['input_slug'];
+        }
         $i++;
       } ?>
       <th scope="col" data-orderable="false" data-searchable="false"></th>
@@ -32,16 +33,19 @@ include_once (ABSOLUTE_PATH.'/admin/header.php');
     $post = $dash::get_content($arr['id']);
     echo '<tr><th scope="row">'.$post['id'].'</th>';
     foreach ($types[$type]['modules'] as $module) {
-      $module_input_slug=$module['input_slug'];
+      if (!in_array($module['input_slug'], $displayed_field_slugs)) {
+        $module_input_slug=$module['input_slug'];
 
-      $module_input_slug_arr=array();
-      if (is_array($module_input_lang))
-        $module_input_slug_arr=$module_input_lang;
-      else
-        $module_input_slug_arr[0]['slug']='';
-      $module_input_slug_lang=$module_input_slug.($module_input_slug_arr[0]['slug']?'_'.$module_input_slug_arr[0]['slug']:'');
-        
-      echo (isset($module['list_field'])?'<td>'.$post[$module_input_slug_lang].'</td>':'');
+        $module_input_slug_arr=array();
+        if (is_array($module_input_lang))
+          $module_input_slug_arr=$module_input_lang;
+        else
+          $module_input_slug_arr[0]['slug']='';
+        $module_input_slug_lang=$module_input_slug.($module_input_slug_arr[0]['slug']?'_'.$module_input_slug_arr[0]['slug']:'');
+          
+        echo (isset($module['list_field'])?'<td>'.$post[$module_input_slug_lang].'</td>':'');
+        $displayed_field_slugs[]=$module['input_slug'];
+      }
     }
     echo '<td><a href="/admin/edit?type='.$post['type'].'&id='.$post['id'].'"><span class="fas fa-edit"></span></a>&nbsp;<a target="new" href="/'.$post['type'].'/'.$post['slug'].'"><span class="fas fa-external-link-alt"></span></a></td></tr>';
   }
