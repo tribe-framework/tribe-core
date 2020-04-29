63aa8163cf19048be8ae4ca2bbffaa6fae5f966e
akshay / jigmet <akshay.wildfire@pm.me>
2020-04-29 16:25:34 +0530

    paste without format always

--- plugins/typeout/typeout.js
+++ plugins/typeout/typeout.js
@@ -33,6 +33,14 @@ $( document ).ready(function() {
             element.empty();
         }
     });
+
+    [].forEach.call(document.querySelectorAll('div[contenteditable="true"]'), function (el) {
+        el.addEventListener('paste', function(e) {
+            e.preventDefault();
+            var text = e.clipboardData.getData("text/plain");
+            document.execCommand("insertHTML", false, text);
+        }, false);
+    });
 });
 
 function restoreSelection (savedSel) {
