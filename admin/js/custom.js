42404ad71f6b588102bd04c07594cd914c2af442
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-02 22:46:55 +0530

    test paste content style

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -62,7 +62,7 @@ $( document ).ready(function() {
 document.querySelector('div[contenteditable="true"]').addEventListener("paste", function(e) {
     e.preventDefault();
     var text = e.clipboardData.getData("text/html");
-    document.execCommand("insertHTML", false, $(text).find('*').removeAttr('class').html());
+    document.execCommand("insertHTML", false, $(text).find('*').removeAttr('class'));
 });
 
 function process_json_out (data) {
