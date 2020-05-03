b80c5dcee6f44252506f3714ced995b82d3f3a92
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-03 12:23:55 +0530

    listing files in edit page

--- admin/edit.php
+++ admin/edit.php
@@ -264,12 +264,12 @@ if (($_GET['id'] && $post['type']==$type) || !$_GET['id']):
 			<?php
 			$i=0;
 			foreach ($post[$module_input_slug_lang] as $file) {
-				$i++;
 				echo '<p class="file done"><span>'.urldecode(basename($file)).'</span><input type="hidden" name="'.$module_input_slug_lang.'[]" value="'.$file.'">&nbsp;&nbsp;<span class="copy_btn btn btn-sm bg-white" data-clipboard-text="'.$file.'"><span class="fas fa-link"></span>&nbsp;copy</span>&nbsp;&nbsp;<a style="display: inline; padding:8px;" class="btn btn-sm bg-white" href="'.$file.'" target="new"><span class="fas fa-external-link-alt"></span>&nbsp;view</a>'.($module['input_descriptor']?'&nbsp;&nbsp;<button type="button" class="btn btn-sm bg-white m-1" data-toggle="modal" data-target="#'.$module_input_slug_lang.'_descriptor_m_'.$i.'">descriptor</button><div class="modal fade" id="'.$module_input_slug_lang.'_descriptor_m_'.$i.'" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">add file descriptor</h5><button type="button" class="close" data-dismiss="modal" aria-label="close"><span aria-hidden="true">×</span></button></div><div class="modal-body"><textarea name="'.$module_input_slug_lang.'_descriptor[]" class="form-control" placeholder="enter file descriptor">'.$post[$module_input_slug_lang.'_descriptor'][$i].'</textarea></div><div class="modal-footer"><button type="button" class="btn btn-sm btn-secondary" data-dismiss="modal">save</button></div></div></div></div>':'').'</p>';
+				$i++;
 			}
 			?>
 		</div>
-
+		
 		<?php endif; ?>
 
 		<?php if ($module_input_type=='google_map_marker'): ?>
