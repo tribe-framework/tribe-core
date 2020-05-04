7b487d960ab7817b5991f552036a21b8da32f63b
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-04 14:06:11 +0530

    get_menu update

--- includes/theme.class.php
+++ includes/theme.class.php
@@ -122,13 +122,7 @@ class theme {
 
 		if ($items) {
 			$op.='
-			<nav class="navbar '.$css_classes['navbar'].'">
-				<a class="navbar-brand" href="'.$items['logo']['href'].'" title="'.$items['logo']['title'].'">'.(isset($items['logo']['src'])?'<img height="'.$items['logo']['height'].'" src="'.$items['logo']['src'].'">':$items['logo']['name']).'</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav ml-auto mr-0">';			
+			<ul class="nav justify-content-center">';	
 					foreach ($items['menu'] as $item) {
 						if (is_array($item['submenu'])) {
 							$op.='<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" href="#" title="'.$item['title'].'" role="button" data-toggle="dropdown">'.$item['name'].'
@@ -149,53 +143,29 @@ class theme {
 							$op.='</div></li>';
 						}
 						else {
-							$op.='<li class="nav-item active"><a class="nav-link" href="'.$item['href'].'" title="'.$item['title'].'">'.$item['name'].'</a></li>';
+							$op.='<li class="nav-item"><a class="nav-link" href="'.$item['href'].'" title="'.$item['title'].'">'.$item['name'].'</a></li>';
 						}
 
 					}
 					$op.='
-					</ul>
-				</div>
-			</nav>';
+					</ul>';
 		}
 		else {
 			$op.='
-			<nav class="navbar navbar-expand-md navbar-light bg-light">
-				<a class="navbar-brand" href="/">Website</a>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
-				</button>
-
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav mr-auto">
-						<li class="nav-item active">
-							<a class="nav-link" href="#">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Link</a>
-						</li>
-						<li class="nav-item dropdown">
-							<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-							Dropdown
-							</a>
-							<div class="dropdown-menu" aria-labelledby="navbarDropdown">
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="#">Something else here</a>
-							</div>
-							</li>
-							<li class="nav-item">
-							<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-						</li>
-					</ul>
-
-					<form class="form-inline my-2 my-md-0">
-						<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-						<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-					</form>
-				</div>
-			</nav>';
+					<ul class="nav justify-content-center">
+					  <li class="nav-item">
+					    <a class="nav-link active" href="#">Active</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" href="#">Link</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link" href="#">Link</a>
+					  </li>
+					  <li class="nav-item">
+					    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+					  </li>
+					</ul>';
 		}
 		
 		return $op;
