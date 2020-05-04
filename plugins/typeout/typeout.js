7fc28adc7d008c68bded84b3985c9da47ca1f0ff
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-04 11:31:48 +0530

    typeout html paste

--- plugins/typeout/typeout.js
+++ plugins/typeout/typeout.js
@@ -34,14 +34,6 @@ $( document ).ready(function() {
             element.empty();
         }
     });
-
-    [].forEach.call(document.querySelectorAll('div[contenteditable="true"]'), function (el) {
-        el.addEventListener('paste', function(e) {
-            e.preventDefault();
-            var text = e.clipboardData.getData("text/html");
-            document.execCommand("insertHTML", false, text);
-        }, false);
-    });
 });
 
 function restoreSelection (savedSel) {
