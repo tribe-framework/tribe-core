f302c4bbb8a7d11aad04dd8895c8356cfe997f37
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-12 14:46:22 +0530

    handling file upload in curl

--- includes/google.class.php
+++ includes/google.class.php
@@ -21,7 +21,7 @@ class google {
 	}
 
 	function curl_api ($url, $params=array(), $method='GET', $body_params='', $content_type='application/json') {
-		return json_decode(shell_exec("curl ".$method." -H 'Content-type: ".$content_type."' -H 'Authorization: Bearer ".$this->access_token."' --data '".file_get_contents(ABSOLUTE_PATH.'/uploads/2020/05-May/09-Sat/oceans.mp4')."' ".(empty($body_params)?"":"--data '".json_encode($body_params)."'")." '".$url.(empty($params)?'':'?'.http_build_query($params))."'"), true);
+		return json_decode(shell_exec("curl ".$method." -H 'Content-type: ".$content_type."' -H 'Authorization: Bearer ".$this->access_token."' ".(empty($body_params)?"":"--data '".json_encode($body_params)."'")." '".$url.(empty($params)?'':'?'.http_build_query($params))."'"), true);
 	}
 
 	function get_curl_file ($file){
