fc2dee8243bf23321d1616b6694c90e336e0dc20
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 23:33:09 +0530

    dashboard layout

--- admin/header.php
+++ admin/header.php
@@ -14,10 +14,11 @@
 </head>
 
 <body>
-<hr class="hr" style="margin:0 !important;">
+<hr class="hr" style="margin:0 !important; position: fixed;">
 <?php
 if ($menus['main']['logo']['name']) {
 	$admin_menus['admin_menu']['logo']=$menus['main']['logo'];
 	$admin_menus['admin_menu']['logo']['src']='';
+	$menus['admin_menu']['logo']['name']=$menus['main']['logo']['name'].' Dashboard';
 }
 echo $theme->get_navbar_menu($admin_menus['admin_menu'], array('navbar'=>'navbar-expand-md navbar-light bg-primary mb-4 py-0', 'ul'=>'navbar-nav ml-auto mr-0', 'li'=>'nav-item', 'a'=>'nav-link text-white'), '<span class="fas fa-bars"></span>', ''); ?>
