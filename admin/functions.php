4a2f2341d5513e06fe049d21061f72034f8c109c
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 22:36:13 +0530

    admin toolbar

--- admin/functions.php
+++ admin/functions.php
@@ -50,7 +50,7 @@ function list_types($type='') {
 	global $types;
 	$list_types='<div class="btn-group" role="group"><a href="'.BASE_URL.'/admin/" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg"><span class="fa fa-tachometer-alt"></span></a>';
 
-	if ($types) {
+	if ($type) {
 		$list_types.='<button id="types-admin-dropdown" type="button" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg dropdown-toggle" data-toggle="dropdown">'.(isset($type)?ucfirst($types[$type]['plural']):'').'&nbsp;<span class="sr-only">Content types</span></button><div class="dropdown-menu" aria-labelledby="types-admin-dropdown">';
 		foreach ($types as $key => $value) {
 	    	$list_types.='<a class="dropdown-item" href="'.BASE_URL.'/admin/list?type='.$types[$key]['slug'].'">'.ucfirst($types[$key]['plural']).'</a>';
