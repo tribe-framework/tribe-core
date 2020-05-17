c298878593aecada089c466aa925c6b70f520535
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 22:04:48 +0530

    auth page

--- admin/auth.php
+++ admin/auth.php
@@ -80,7 +80,7 @@ body {
 
   <body class="text-center">
     <form class="form-signin">
-  <img class="mb-4" src="https://dash.hrln.org/themes/hrln-2020/assets/images/HRLN_Logo.png" alt="" width="72" height="72">
+  <img class="mb-4" src="https://dash.hrln.org/themes/hrln-2020/assets/images/HRLN_Logo.png" alt="">
   <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
   <label for="inputEmail" class="sr-only">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
