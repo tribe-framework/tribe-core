7b0510e7758a67940c29e495a5e7be6028c4073d
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 22:01:07 +0530

    auth page

--- admin/auth.php
+++ admin/auth.php
@@ -1,6 +1,98 @@
 <?php
-include_once ('../config-init.php');
-include_once (ABSOLUTE_PATH.'/admin/header.php');
-?>
+include_once ('../config-init.php'); ?>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title><?php echo 'Wildfire Dashboard'.(isset($headmeta_title)?' / '.$headmeta_title:''); ?></title>
+	<meta name="description" content="Wildfire admin dashboard for <?php echo (isset($headmeta_title)?' / '.$headmeta_title:''); ?>">
+	<link rel="stylesheet" href="https://use.typekit.net/xkh7dxd.css">
+	<link href="<?php echo BASE_URL; ?>/admin/css/bootstrap.min.css" rel="stylesheet">
+	<link href="<?php echo BASE_URL; ?>/admin/css/wildfire.css" rel="stylesheet">
+	<link href="/plugins/fontawesome/css/all.min.css" rel="stylesheet">
+	<link href="/plugins/datatables/datatables.min.css" rel="stylesheet">
+	<link href="<?php echo BASE_URL; ?>/admin/css/custom.css?v=<?php echo time(); ?>" rel="stylesheet">
+</head>
 
+<body>
+<meta name="theme-color" content="#563d7c">
+
+<style>
+  .bd-placeholder-img {
+    font-size: 1.125rem;
+    text-anchor: middle;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+  }
+
+  @media (min-width: 768px) {
+    .bd-placeholder-img-lg {
+      font-size: 3.5rem;
+    }
+  }
+html,
+body {
+  height: 100%;
+}
+
+body {
+  display: -ms-flexbox;
+  display: flex;
+  -ms-flex-align: center;
+  align-items: center;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  background-color: #f5f5f5;
+}
+
+.form-signin {
+  width: 100%;
+  max-width: 330px;
+  padding: 15px;
+  margin: auto;
+}
+.form-signin .checkbox {
+  font-weight: 400;
+}
+.form-signin .form-control {
+  position: relative;
+  box-sizing: border-box;
+  height: auto;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+
+    </style>
+
+  <div class="text-center">
+    <form class="form-signin">
+  <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+  <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
+  <label for="inputEmail" class="sr-only">Email address</label>
+  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+  <div class="checkbox mb-3">
+    <label>
+      <input type="checkbox" value="remember-me"> Remember me
+    </label>
+  </div>
+  <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+  <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+</form>
+</div>
 <?php include_once (ABSOLUTE_PATH.'/admin/footer.php'); ?>
