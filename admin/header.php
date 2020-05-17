c0446a686783a901ba4f3c38d9b1310fb40f9441
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 16:14:13 +0530

    wildfire css bootstrap

--- admin/header.php
+++ admin/header.php
@@ -6,6 +6,7 @@
 	<title><?php echo 'Wildfire'.(isset($headmeta_title)?' / '.$headmeta_title:''); ?></title>
 	<meta name="description" content="Wildfire admin dashboard for <?php echo (isset($headmeta_title)?' / '.$headmeta_title:''); ?>">
 
+	<link href="<?php echo BASE_URL; ?>/admin/css/wildfire.bootstrap.css" rel="stylesheet">
 	<link href="<?php echo BASE_URL; ?>/admin/css/wildfire.css" rel="stylesheet">
 	<link href="/plugins/fontawesome/css/all.min.css" rel="stylesheet">
 	<link href="/plugins/datatables/datatables.min.css" rel="stylesheet">
