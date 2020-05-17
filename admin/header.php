46cf68e435c4e98e492ae7c40c87b60789d9566f
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 21:07:06 +0530

    admin menu

--- admin/header.php
+++ admin/header.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title><?php echo 'Wildfire'.(isset($headmeta_title)?' / '.$headmeta_title:''); ?></title>
+	<title><?php echo 'Wildfire Dashboard'.(isset($headmeta_title)?' / '.$headmeta_title:''); ?></title>
 	<meta name="description" content="Wildfire admin dashboard for <?php echo (isset($headmeta_title)?' / '.$headmeta_title:''); ?>">
 	<link rel="stylesheet" href="https://use.typekit.net/xkh7dxd.css">
 	<link href="<?php echo BASE_URL; ?>/admin/css/bootstrap.min.css" rel="stylesheet">
@@ -14,4 +14,4 @@
 </head>
 
 <body>
-<?php echo $theme->get_navbar_menu($admin_menus['admin_menu'], array('navbar'=>'navbar-expand-md navbar-dark bg-dark p-3', 'ul'=>'navbar-nav ml-auto mr-0', 'li'=>'nav-item', 'a'=>'nav-link text-white'), '<span class="fas fa-bars"></span>'); ?>
+<?php echo $theme->get_navbar_menu($admin_menus['admin_menu'], array('navbar'=>'navbar-expand-md navbar-dark bg-dark p-4 pt-3', 'ul'=>'navbar-nav ml-auto mr-0', 'li'=>'nav-item', 'a'=>'nav-link text-white'), '<span class="fas fa-bars"></span>'); ?>
