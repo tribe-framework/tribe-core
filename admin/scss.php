fe34003d4d19ef79049f1adecedcfcfb4348a2d5
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 18:48:20 +0530

    wildfire css bootstrap

--- admin/scss.php
+++ admin/scss.php
@@ -1,6 +1,7 @@
 <?php
 include_once ('../config-init.php');
 $sass = new Sass();
+$sass->setStyle(Sass::STYLE_COMPRESSED);
 $css = $sass->compileFile(ABSOLUTE_PATH.'/plugins/bootstrap/scss/bootstrap.scss');
 echo $css;
 ?>
