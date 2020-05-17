2d9cd092b7f01f0b0f9928dba0895c8a2af92231
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 22:03:35 +0530

    auth page

--- admin/auth.php
+++ admin/auth.php
@@ -78,7 +78,7 @@ body {
 
     </style>
 
-  <div class="text-center">
+  <body class="text-center">
     <form class="form-signin">
   <img class="mb-4" src="/docs/4.4/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
   <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
@@ -94,5 +94,5 @@ body {
   <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
   <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
 </form>
-</div>
-<?php include_once (ABSOLUTE_PATH.'/admin/footer.php'); ?>
+</body>
+</html>
