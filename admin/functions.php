ad24b1b8fd5a32a7a73a72f8e07744b70de0e7e9
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-17 20:09:08 +0530

    wildfire design

--- admin/functions.php
+++ admin/functions.php
@@ -41,16 +41,16 @@ function new_and_list ($type) {
 	global $types;
 	return '
 	<div class="btn-group">
-		<a href="/admin/edit?type='.$type.'" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg"><span class="fa fa-edit"></span>&nbsp;New</a>
-		<a href="/admin/list?type='.$type.'" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg"><span class="fa fa-list"></span>&nbsp;List</a>
+		<a href="'.BASE_URL.'/admin/edit?type='.$type.'" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg"><span class="fa fa-edit"></span>&nbsp;New</a>
+		<a href="'.BASE_URL.'/admin/list?type='.$type.'" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg"><span class="fa fa-list"></span>&nbsp;List</a>
 	</div>';
 }
 
 function list_types($type='') {
 	global $types;
-	$list_types='<div class="btn-group" role="group"><a href="/admin/" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg"><span class="fa fa-tachometer-alt"></span></a><button id="types-admin-dropdown" type="button" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg dropdown-toggle px-1" data-toggle="dropdown">'.(isset($type)?ucfirst($types[$type]['plural']):'').'&nbsp;<span class="sr-only">Content types</span></button><div class="dropdown-menu" aria-labelledby="types-admin-dropdown">';
+	$list_types='<div class="btn-group" role="group"><a href="'.BASE_URL.'" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg"><span class="fa fa-igloo"></span></a><a href="'.BASE_URL.'/admin/" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg"><span class="fa fa-tachometer-alt"></span></a><button id="types-admin-dropdown" type="button" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 btn-lg dropdown-toggle px-1" data-toggle="dropdown">'.(isset($type)?ucfirst($types[$type]['plural']):'').'&nbsp;<span class="sr-only">Content types</span></button><div class="dropdown-menu" aria-labelledby="types-admin-dropdown">';
 	foreach ($types as $key => $value) {
-    	$list_types.='<a class="dropdown-item" href="/admin/list?type='.$types[$key]['slug'].'">'.ucfirst($types[$key]['plural']).'</a>';
+    	$list_types.='<a class="dropdown-item" href="'.BASE_URL.'/admin/list?type='.$types[$key]['slug'].'">'.ucfirst($types[$key]['plural']).'</a>';
 	}
 	$list_types.='</div></div>';
 	return $list_types;
