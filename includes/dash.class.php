ace89b828e6bfdec7e36d2c745241c89c171528a
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-18 15:41:57 +0530

    wp import

--- includes/dash.class.php
+++ includes/dash.class.php
@@ -219,7 +219,17 @@ class dash {
 			    $post['content_privacy']='public';
 			    $post['publishing_date']=substr($r['post_date'], 0, 10);
 			    $cv=$sql->executeSQL("SELECT `guid` FROM `".$wp_table_name."` WHERE `post_parent` != 0 AND `guid` LIKE '%wp-content/uploads%' AND `post_type` LIKE 'attachment' AND `post_status` LIKE 'inherit' AND `guid` != '' AND `post_parent`='".$r['ID']."' ORDER BY `ID` DESC");
-			    $post['cover_media']=$cv[0]['guid'];
+			    foreach ($cv as $file) {
+			    	$post['files']=array();
+			    	$ext=substr($file['guid'], -4);
+			    	if ($ext=='.pdf') {
+			    		$post['files'][]=$file['guid'];
+			    	}
+			    	else {
+			    		$post['files'][]=$file['guid'];
+			    		$post['cover_media']=$file['guid'];
+			    	}
+			    }
 			    $this->push_content($post);
 
 			    $i++;
