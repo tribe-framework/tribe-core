96d0c9e1c5c138fab5e3642b42179067de098665
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-18 12:35:29 +0530

    dashboard layout

--- admin/index.php
+++ admin/index.php
@@ -9,22 +9,15 @@ include_once (ABSOLUTE_PATH.'/admin/header.php');
 
 <div class="row m-0">
 <div class="card col-md my-2">
+  <div class="card-header">Analytics</div>
   <div class="card-body">
-    <h5 class="card-title">Dashboard widget 1</h5>
     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
   </div>
 </div>
 <div class="card col-md my-2">
+  <div class="card-header">Latest</div>
   <div class="card-body">
-    <h5 class="card-title">Dashboard widget 2</h5>
-    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-  </div>
-</div>
-<div class="card col-md my-2">
-  <div class="card-body">
-    <h5 class="card-title">Dashboard widget 2</h5>
     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
   </div>
