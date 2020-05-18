2b735c82444043b7c62139d6928ecbd73f6acbc5
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-18 13:35:41 +0530

    wp import

--- includes/dash.class.php
+++ includes/dash.class.php
@@ -208,22 +208,24 @@ class dash {
 		$i=0;
 		$q=$sql->executeSQL("SELECT * FROM `".$wp_table_name."` WHERE `post_status` LIKE 'publish' AND `post_parent` = 0 AND `post_type` LIKE 'post'");
 		foreach ($q as $r) {
-			$post=array();
-			$post['wp_import']=1;
-		    $post['id']=$r['ID'];
-		    $post['type']=$type;
-		    $post['title']=$r['post_title'];
-		    $post['body']=$r['post_content'];
-		    $post['slug']=$r['post_name'];
-		    $post['content_privacy']='public';
-		    $post['publishing_date']=substr($r['post_date'], 0, 10);
-		    $cv=$sql->executeSQL("SELECT `guid` FROM `".$wp_table_name."` WHERE `post_parent` != 0 AND `guid` LIKE '%wp-content/uploads%' AND `post_type` LIKE 'attachment' AND `post_status` LIKE 'inherit' AND `guid` != '' AND `post_parent`='".$r['ID']."' ORDER BY `ID` DESC");
-		    $post['cover_media']=$cv[0]['guid'];
-		    dash::push_content($post);
+			if (!$this->get_content_meta($r['ID'], 'slug')) {
+				$post=array();
+				$post['wp_import']=1;
+			    $post['id']=$r['ID'];
+			    $post['type']=$type;
+			    $post['title']=$r['post_title'];
+			    $post['body']=$r['post_content'];
+			    $post['slug']=$r['post_name'];
+			    $post['content_privacy']='public';
+			    $post['publishing_date']=substr($r['post_date'], 0, 10);
+			    $cv=$sql->executeSQL("SELECT `guid` FROM `".$wp_table_name."` WHERE `post_parent` != 0 AND `guid` LIKE '%wp-content/uploads%' AND `post_type` LIKE 'attachment' AND `post_status` LIKE 'inherit' AND `guid` != '' AND `post_parent`='".$r['ID']."' ORDER BY `ID` DESC");
+			    $post['cover_media']=$cv[0]['guid'];
+			    $this->push_content($post);
 
-		    $i++;
-			if ($max_records && $i>=$max_records)
-				break;
+			    $i++;
+				if ($max_records && $i>=$max_records)
+					break;
+			}
 		}
 	}
 }
