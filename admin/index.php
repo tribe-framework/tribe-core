efd489f6e70979a5c3bcd915999d3314040c278f
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-18 12:37:19 +0530

    dashboard layout

--- admin/index.php
+++ admin/index.php
@@ -7,15 +7,15 @@ include_once (ABSOLUTE_PATH.'/admin/header.php');
 
 <?php echo get_admin_menu('dash'); ?>
 
-<div class="row m-0">
-<div class="card col-md my-2">
+<div class="card-group m-0">
+<div class="card my-2">
   <div class="card-header">Analytics</div>
   <div class="card-body">
     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
     <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
   </div>
 </div>
-<div class="card col-md my-2">
+<div class="card my-2">
   <div class="card-header">Latest</div>
   <div class="card-body">
     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
