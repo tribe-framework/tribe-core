11b643ad3063d8f1f7404a5e3001a3a400a539d9
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-18 13:39:03 +0530

    wp import

--- includes/dash.class.php
+++ includes/dash.class.php
@@ -219,6 +219,7 @@ class dash {
 			    $post['content_privacy']='public';
 			    $post['publishing_date']=substr($r['post_date'], 0, 10);
 			    $cv=$sql->executeSQL("SELECT `guid` FROM `".$wp_table_name."` WHERE `post_parent` != 0 AND `guid` LIKE '%wp-content/uploads%' AND `post_type` LIKE 'attachment' AND `post_status` LIKE 'inherit' AND `guid` != '' AND `post_parent`='".$r['ID']."' ORDER BY `ID` DESC");
+			    echo "SELECT `guid` FROM `".$wp_table_name."` WHERE `post_parent` != 0 AND `guid` LIKE '%wp-content/uploads%' AND `post_type` LIKE 'attachment' AND `post_status` LIKE 'inherit' AND `guid` != '' AND `post_parent`='".$r['ID']."' ORDER BY `ID` DESC";
 			    $post['cover_media']=$cv[0]['guid'];
 			    $this->push_content($post);
 
