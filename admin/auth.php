3cdc9201858d6734d2e2cf2e340b46db5bc98bdf
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-18 10:11:06 +0530

    auth layout

--- admin/auth.php
+++ admin/auth.php
@@ -80,7 +80,7 @@ body {
 
   <body class="text-center">
     <form class="form-signin">
-  <img class="mb-4 w-100" src="https://dash.hrln.org/themes/hrln-2020/assets/images/HRLN_Logo.png">
+  <img class="mb-4 w-100" src="img/logo-flame.png">
   <h1 class="h3 mb-3 font-weight-normal">Please sign in</h1>
   <label for="inputEmail" class="sr-only">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
@@ -92,7 +92,7 @@ body {
     </label>
   </div>
   <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-  <p class="mt-5 mb-3 text-muted">&copy; 2017-2019</p>
+  <p class="text-muted small my-3 pr-5">&copy; <?php echo (date('Y')=='2020'?date('Y'):'2020 - '.date('Y')); ?> Wildfire</p>
 </form>
 </body>
 </html>
