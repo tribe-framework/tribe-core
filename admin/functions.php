6395b008528c6ba8b575995087decbf175a15f1d
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-20 12:03:07 +0530

    dashboard layout

--- admin/functions.php
+++ admin/functions.php
@@ -58,15 +58,16 @@ function list_types($type='') {
 		$list_types.='</div></div>';
 	}
 	else {
-		$list_types.='</div><button id="types-admin-dropdown" type="button" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 dropdown-toggle d-md-none" data-toggle="dropdown">'.(isset($type)?ucfirst($types[$type]['plural']):'').'&nbsp;Content types</button><div class="dropdown-menu" aria-labelledby="types-admin-dropdown">';
+		$list_types.='<button id="types-admin-dropdown" type="button" class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0 dropdown-toggle d-md-none" data-toggle="dropdown">'.(isset($type)?ucfirst($types[$type]['plural']):'').'&nbsp;Content types</button><div class="dropdown-menu" aria-labelledby="types-admin-dropdown">';
 		foreach ($types as $key => $value) {
 	    	$list_types.='<a class="dropdown-item" href="'.BASE_URL.'/admin/list?type='.$types[$key]['slug'].'">'.ucfirst($types[$key]['plural']).'</a>';
 		}
+		$list_types.='</div><div class="btn-group d-none d-md-block" role="group">';
 		foreach ($types as $key => $value) {
 			if ($types[$key]['slug'])
 		    	$list_types.='<a class="btn btn-outline-primary border-top-0 border-left-0 border-right-0 rounded-0" href="'.BASE_URL.'/admin/list?type='.$types[$key]['slug'].'">'.ucfirst($types[$key]['plural']).'</a>';
 		}
-		$list_types.='</div>';
+		$list_types.='</div></div>';
 	}
 	return $list_types;
 }
