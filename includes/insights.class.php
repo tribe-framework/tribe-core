a83d2b9e3da9b88557c8551f20c9ea2809fa51dc
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-22 08:33:08 +0530

    insights code

---
+++ includes/insights.class.php
@@ -0,0 +1,20 @@
+<?php
+/*
+	functions start with push_, pull_, get_, do_ or is_
+	push_ is to save to database
+	pull_ is to pull from database, returns 1 or 0, saves the output array in $last_data
+	get_ is to get usable values from functions
+	do_ is for action that doesn't have a database push or pull
+	is_ is for a yes/no answer
+*/
+
+class insights {
+
+	function __construct () {
+		
+	}
+
+	
+}
+
+?>
