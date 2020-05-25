06d3d76053e1106df1d99b2dd6492d0ece929466
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-25 08:22:02 +0530

    auth page layout

--- admin/auth.php
+++ admin/auth.php
@@ -3,7 +3,7 @@ include_once ('../config-init.php'); ?>
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<title><?php echo 'Wildfire Entity'.(isset($headmeta_title)?' &raquo; '.$headmeta_title:''); ?></title>
+	<title><?php echo (isset($headmeta_title)?$headmeta_title.' &raquo; ':'').'Wildfire Entity'; ?></title>
 	<meta name="description" content="Access authorisation<?php echo (isset($headmeta_title)?' for '.$headmeta_title:''); ?>">
 	<link rel="stylesheet" href="https://use.typekit.net/xkh7dxd.css">
 	<link href="<?php echo BASE_URL; ?>/admin/css/bootstrap.min.css" rel="stylesheet">
@@ -17,8 +17,19 @@ include_once ('../config-init.php'); ?>
 
 <body class="text-center">
   <hr class="hr fixed-top" style="margin:0 !important;">
-  <form class="form-signin">
+  <form>
   <h2><?php echo $menus['main']['logo']['name']; ?></h2>
+
+<?php if ($_GET['section']=='exit'): ?>
+
+<?php elseif ($_GET['section']=='register'): ?>
+
+<?php elseif ($_GET['section']=='change-password'): ?>
+
+<?php elseif ($_GET['section']=='profile'): ?>
+
+<?php elseif ($_GET['section']=='forgot-password'): ?>
+
   <h4 class="my-3 font-weight-normal"><span class="fas fa-sign-in-alt"></span>&nbsp;Sign in</h4>
   <label for="inputEmail" class="sr-only">Email address</label>
   <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
@@ -31,8 +42,22 @@ include_once ('../config-init.php'); ?>
   </div>
   <a class="btn btn-sm btn-primary btn-block" href="/admin">Sign in</a>
   <a class="btn btn-sm btn-primary btn-block" href="/admin">Register</a>
-  <p class="text-muted small my-5"><?php echo '<a href="'.BASE_URL.'"><span class="fas fa-angle-double-left"></span>&nbsp;'.$menus['main']['logo']['name'].'</a>'; ?></p>
-  <p class="text-muted small my-5">&copy; <?php echo (date('Y')=='2020'?date('Y'):'2020 - '.date('Y')); ?> Wildfire</p>
+
+<?php else: ?>
+
+  <h4 class="my-3 font-weight-normal"><span class="fas fa-lock"></span>&nbsp;Sign in</h4>
+  <label for="inputEmail" class="sr-only">Email address</label>
+  <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+  <label for="inputPassword" class="sr-only">Password</label>
+  <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+  <a class="btn btn-sm btn-primary btn-block" href="/admin">Sign in</a>
+  <a class="btn btn-sm btn-primary btn-block" href="/admin">Register</a>
+  <p class="text-muted small my-1"><?php echo '<a href="'.BASE_URL.'"><span class="fas fa-key"></span>&nbsp;Forgot password?</a>'; ?></p>
+
+<?php endif; ?>
+
+    <p class="text-muted small my-5"><?php echo '<a href="'.BASE_URL.'"><span class="fas fa-angle-double-left"></span>&nbsp;'.$menus['main']['logo']['name'].'</a>'; ?></p>
+    <p class="text-muted small my-5">&copy; <?php echo (date('Y')=='2020'?date('Y'):'2020 - '.date('Y')); ?> Wildfire</p>
   </form>
 </body>
 </html>
