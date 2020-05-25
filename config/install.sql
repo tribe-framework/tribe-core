b2af3c047502e0850db535a60439ecddf3e85f94
akshay / wildfire <akshay.wildfire@pm.me>
2020-05-25 23:34:17 +0530

    updated sql removed auth table added trac table removed user_id column in data table

--- config/install.sql
+++ config/install.sql
@@ -1,73 +1,31 @@
--- phpMyAdmin SQL Dump
--- version 4.9.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost
--- Generation Time: Apr 09, 2020 at 09:52 AM
--- Server version: 5.7.29-0ubuntu0.18.04.1
--- PHP Version: 7.2.24-0ubuntu0.18.04.3
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
--- --------------------------------------------------------
-
---
--- Table structure for table `auth`
---
-
-CREATE TABLE `auth` (
-  `id` bigint(20) NOT NULL,
-  `user` json DEFAULT NULL,
-  `updated_on` bigint(20) DEFAULT NULL,
-  `created_on` bigint(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `data`
---
-
 CREATE TABLE `data` (
   `id` bigint(20) NOT NULL,
   `content` json DEFAULT NULL,
   `updated_on` bigint(20) DEFAULT NULL,
-  `created_on` bigint(20) NOT NULL,
-  `user_id` bigint(20) NOT NULL
+  `created_on` bigint(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `auth`
---
-ALTER TABLE `auth`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `data`
---
 ALTER TABLE `data`
   ADD PRIMARY KEY (`id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `auth`
---
-ALTER TABLE `auth`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `data`
---
 ALTER TABLE `data`
   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+CREATE TABLE `trac` (
+  `id` bigint(20) NOT NULL,
+  `visit` json DEFAULT NULL,
+  `created_on` bigint(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+ALTER TABLE `trac`
+  ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `trac`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
 COMMIT;
