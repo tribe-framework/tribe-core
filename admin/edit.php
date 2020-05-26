93355e814e4e70406ca5302e5f051eb8f8522361
akshay / jigmet <akshay.wildfire@pm.me>
2020-05-26 20:09:22 +0530

    combining edit and registration form php code

--- admin/edit.php
+++ admin/edit.php
@@ -1,7 +1,40 @@
 <?php
 include_once ('../config-init.php');
 include_once (ABSOLUTE_PATH.'/admin/header.php'); ?>
+<?php
+if ($_GET['id'])
+	$post = $dash::get_content($_GET['id']);
 
-<?php include_once (ABSOLUTE_PATH.'/admin/form.php'); ?>
+if ($_GET['role'])
+	$role = $types['user']['roles'][$_GET[role]];
 
+if (($_GET['id'] && $post['type']==$type) || !$_GET['id']):
+
+	//for testing resticted min and max ids for archive format changes
+	if (!($pid=$_GET['id']))
+		$pid=$dash::get_next_id();
+?>
+
+	<link rel="stylesheet" type="text/css" href="/plugins/typeout/typeout.css">
+
+	<div class="p-3">
+	<a name="infos"></a><div id="infos" class="d-none alert alert-info"></div>
+	<a name="errors"></a><div id="errors" class="d-none alert alert-danger"></div>
+
+	<form method="post" class="edit_form" action="/admin/json" autocomplete="off">
+
+		<?php echo get_admin_menu('edit', $type, $role['slug'], $_GET['id']); ?>
+
+		<h2 class="form_title"><?php echo ($type=='user'?$role['title'].'&nbsp;<small><span class="fas fa-angle-double-right"></span></small>&nbsp;':'').'Edit '.$types[$type]['name']; ?></h2>
+
+		<?php include_once (ABSOLUTE_PATH.'/admin/form.php'); ?>
+
+		<?php if (count($types[$type]['modules'])>3) { echo get_admin_menu('edit', $type, $role['slug'], $_GET['id']); } ?>
+		<p>&nbsp;</p>
+	
+	</form>
+
+	</div>
+		
+<?php endif; ?>
 <?php include_once (ABSOLUTE_PATH.'/admin/footer.php'); ?>
--- admin/form.php
+++ admin/form.php
@@ -1,28 +1,3 @@
-<?php
-if ($_GET['id'])
-	$post = $dash::get_content($_GET['id']);
-
-if ($_GET['role'])
-	$role = $types['user']['roles'][$_GET[role]];
-
-if (($_GET['id'] && $post['type']==$type) || !$_GET['id']):
-
-	//for testing resticted min and max ids for archive format changes
-	if (!($pid=$_GET['id']))
-		$pid=$dash::get_next_id();
-?>
-
-	<link rel="stylesheet" type="text/css" href="/plugins/typeout/typeout.css">
-
-	<div class="p-3">
-	<a name="infos"></a><div id="infos" class="d-none alert alert-info"></div>
-	<a name="errors"></a><div id="errors" class="d-none alert alert-danger"></div>
-
-	<form method="post" class="edit_form" action="/admin/json" autocomplete="off">
-
-		<?php echo get_admin_menu('edit', $type, $role['slug'], $_GET['id']); ?>
-
-		<h2 class="form_title"><?php echo ($type=='user'?$role['title'].'&nbsp;<small><span class="fas fa-angle-double-right"></span></small>&nbsp;':'').'Edit '.$types[$type]['name']; ?></h2>
 
 		<?php foreach ($types[$type]['modules'] as $module) {
 			if ((!$module['restrict_id_max'] || $pid<=$module['restrict_id_max']) && (!$module['restrict_id_min'] || $pid>=$module['restrict_id_min'])):
@@ -446,37 +421,29 @@ if (($_GET['id'] && $post['type']==$type) || !$_GET['id']):
 		<input type="hidden" name="type" value="<?php echo $types[$type]['slug']; ?>">
 		<input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
 		<input type="hidden" name="slug" value="<?php echo $post['slug']; ?>">
-		
-		<?php if (count($types[$type]['modules'])>3) { echo get_admin_menu('edit', $type, $role['slug'], $_GET['id']); } ?>
-		<p>&nbsp;</p>
-	</form>
 
-	</div>
-
-	<div class="modal fade" id="delete_conf_<?php echo $_GET['id']; ?>" tabindex="-1" role="dialog">
-	  <div class="modal-dialog" role="document">
-	    <div class="modal-content">
-	      <div class="modal-header">
-	        <h5 class="modal-title">Confirm</h5>
-	        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-	          <span aria-hidden="true">&times;</span>
-	        </button>
-	      </div>
-	      <div class="modal-body">
-	        Are you sure you wish to delete this content?
-	      </div>
-	      <div class="modal-footer">
-	        <form method="post" class="edit_form" action="/admin/json">
-	          <input type="hidden" name="class" value="dash">
-	          <input type="hidden" name="function" value="do_delete">
-	          <input type="hidden" name="type" value="<?php echo $types[$type]['slug']; ?>">
-	          <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
-	          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
-	          <button type="submit" class="btn btn-danger">Yes, delete it</button>
-	        </form>
-	      </div>
-	    </div>
-	  </div>
-	</div>
-		
-<?php endif; ?>
+		<div class="modal fade" id="delete_conf_<?php echo $_GET['id']; ?>" tabindex="-1" role="dialog">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <h5 class="modal-title">Confirm</h5>
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+		          <span aria-hidden="true">&times;</span>
+		        </button>
+		      </div>
+		      <div class="modal-body">
+		        Are you sure you wish to delete this content?
+		      </div>
+		      <div class="modal-footer">
+		        <form method="post" class="edit_form" action="/admin/json">
+		          <input type="hidden" name="class" value="dash">
+		          <input type="hidden" name="function" value="do_delete">
+		          <input type="hidden" name="type" value="<?php echo $types[$type]['slug']; ?>">
+		          <input type="hidden" name="id" value="<?php echo $_GET['id']; ?>">
+		          <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+		          <button type="submit" class="btn btn-danger">Yes, delete it</button>
+		        </form>
+		      </div>
+		    </div>
+		  </div>
+		</div>
