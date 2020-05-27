6bdaced569def72a8b9d04bfe386a4b134744eaf
akshay / wildfire <akshay.wildfire@pm.me>
2020-05-27 14:13:08 +0530

    scss for themes

--- admin/scss.php
+++ admin/scss.php
@@ -3,6 +3,6 @@ include_once ('../config-init.php');
 $sass = new Sass();
 $sass->setStyle(Sass::STYLE_COMPRESSED);
 $sass->setEmbed(true);
-$css = $sass->compileFile(ABSOLUTE_PATH.'/plugins/bootstrap/scss/bootstrap.scss');
+$css = $sass->compileFile(THEME_PATH.'/scss/bootstrap.scss');
 echo $css;
 ?>
