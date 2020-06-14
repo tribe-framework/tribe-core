07dc263fff5d1e13e6d4800143e551289b19a935
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-14 12:30:11 +0530

    removed map from dashboard

--- admin/index.php
+++ admin/index.php
@@ -7,39 +7,6 @@ include_once (ABSOLUTE_PATH.'/admin/header.php');
 
 <?php echo get_admin_menu('dash'); ?>
 
-<?php
-$op='';
-$postids=$dash::get_all_ids('lane');
-if ($postids) {
-  echo '<div class="card my-2"><div class="card-header">Patparganj Constituency</div><div class="card-body p-0"><div id="map" style="height: 500px"></div>';
-  foreach ($postids as $postarr) {
-    $pdata=$dash::get_content($postarr['id']);
-    $i++;
-    $op.='
-      var flightPlanCoordinates_'.$i.' = [
-      {lat: '.$pdata['start_lat'].', lng: '.$pdata['start_lng'].'},
-      {lat: '.$pdata['end_lat'].', lng: '.$pdata['end_lng'].'}
-    ];
-    var flightPath_'.$i.' = new google.maps.Polyline({
-      path: flightPlanCoordinates_'.$i.',
-      strokeColor: \'#00aaee\',
-      strokeOpacity: 1.0,
-      strokeWeight: 10
-    });
-    flightPath_'.$i.'.setMap(map);';
-  }
-  ?>
-  <script type="text/javascript"> 
-  function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-      zoom: 16,
-      center: <?php echo '{lat: 28.6310992, lng: 77.2883984}'; ?>
-    });
-    <?php echo $op; ?>
-  }
-  </script>
-<?php echo '</div></div>'; } ?>
-
 <div class="card-group m-0">
 <div class="card my-2">
   <div class="card-header">Analytics</div>
