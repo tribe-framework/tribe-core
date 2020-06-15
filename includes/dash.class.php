884b2091aeb0d66f856ee2b75d355c925d10336b
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-15 15:01:41 +0530

    update meta_values in dash function

--- includes/dash.class.php
+++ includes/dash.class.php
@@ -134,8 +134,10 @@ class dash {
 	function push_content_meta ($id, $meta_key, $meta_value) {
 		global $sql;
 		if ($id && $meta_key) {
-			if (!trim($meta_value))
+			if (!trim($meta_value)) {
+				echo "UPDATE `data` SET `content` = JSON_REMOVE(`content`, '$.".$meta_key."') WHERE `id`='$id'";
 				$q=$sql->executeSQL("UPDATE `data` SET `content` = JSON_REMOVE(`content`, '$.".$meta_key."') WHERE `id`='$id'");
+			}
 			else
 				$q=$sql->executeSQL("UPDATE `data` SET `content` = JSON_SET(`content`, '$.".$meta_key."', '$meta_value') WHERE `id`='$id'");
 			return 1;
