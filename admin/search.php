7ae5339f2cf78f7c68ba6e189a16fd124fd709f1
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-20 15:41:05 +0530

    json search data file

--- admin/search.php
+++ admin/search.php
@@ -1,11 +1,10 @@
 <?php
-//header('Content-Type: application/json');
+header('Content-Type: application/json');
 include_once ('../config-init.php');
 $or=array();
 if ($types['webapp']['searchable_types'])
-	$or['data']=$sql->executeSQL("SELECT `id`, CONCAT('<a href=\"/', `content`->>'$.type', '/', `content`->>'$.slug', '\" target=\"new\">', `content`->>'$.title', '</a> <div class=\"d-none\">', `content`->>'$.view_searchable_data', '</div>') `result` FROM `data` WHERE `content`->'$.content_privacy'='public' && `content`->'$.type' IN ('".join("', '", $types['webapp']['searchable_types'])."') ORDER BY `id` DESC");
-echo "SELECT `id`, CONCAT('<a href=\"/', `content`->>'$.type', '/', `content`->>'$.slug', '\" target=\"new\">', `content`->>'$.title', '</a> <div class=\"d-none\">', `content`->>'$.view_searchable_data', '</div>') `result` FROM `data` WHERE `content`->'$.content_privacy'='public' && `content`->'$.type' IN ('".join("', '", $types['webapp']['searchable_types'])."') ORDER BY `id` DESC"
-//echo json_encode($or);
+	$or['data']=$sql->executeSQL("SELECT `id`, CONCAT('<a href=\"/', `content`->>'$.type', '/', `content`->>'$.slug', '\" target=\"new\">', `content`->>'$.title', '</a> <div class=\"d-none\">', `content`->>'$.view_searchable_data', '</div>') `result` FROM `data` WHERE `content`->'$.content_privacy'='public' && `content`->'$.type'!='' ORDER BY `id` DESC");
+echo json_encode($or);
 /* code for converting existing data into searchable
 include_once(ABSOLUTE_PATH.'/includes/dash.class.php');
 $dash = new dash();
