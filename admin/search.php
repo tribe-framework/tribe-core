2dbce6800b7287e6e8c1ea3f4754b847d7110713
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-20 15:35:48 +0530

    json search data file

--- admin/search.php
+++ admin/search.php
@@ -1,15 +1,13 @@
 <?php
 header('Content-Type: application/json');
 include_once ('../config-init.php');
-
+$or=array();
+if ($types['webapp']['searchable_types'])
+	$or['data']=$sql->executeSQL("SELECT `id`, CONCAT('<a href=\"/', `content`->>'$.type', '/', `content`->>'$.slug', '\" target=\"new\">', `content`->>'$.title', '</a> <div class=\"d-none\">', `content`->>'$.view_searchable_data', '</div>') `result` FROM `data` WHERE `content`->'$.content_privacy'='public' && `content`->'$.type' IN ('".join(', ', $types['webapp']['searchable_types'])."')");
+echo json_encode($or);
+/* code for converting existing data into searchable
 include_once(ABSOLUTE_PATH.'/includes/dash.class.php');
 $dash = new dash();
-
-$or=array();
-$or['data']=$sql->executeSQL("SELECT `id`, CONCAT('<a href=\"/', `content`->>'$.type', '/', `content`->>'$.slug', '\" target=\"new\">', `content`->>'$.title', '</a> <div class=\"d-none\">', `content`->>'$.view_searchable_data', '</div>') `result` FROM `data` WHERE `content`->'$.content_privacy'='public'");
-echo json_encode($or);
-
-/* code for converting existing data into searchable
 $q=$sql->executeSQL("SELECT `id` FROM `data` WHERE 1");
 foreach ($q as $r) {
 	$og=$dash->get_content($r['id']);
