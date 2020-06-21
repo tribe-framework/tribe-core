eb5b7c8aa3c82ea98552782dee791de31b2166d7
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 20:46:34 +0530

    sitemap.xml

--- sitemap.php
+++ sitemap.php
@@ -2,22 +2,24 @@
 <?php header('Content-type: application/xml; charset=utf-8'); ?>
 <?php
 $or=array();
-$or[]=array('url'=>array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1'));
 $xml = new SimpleXMLElement('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
+$or[0]['url']=array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1');
 array_to_xml($or, $xml);
 print $xml->asXML();
 
 function array_to_xml( $data, &$xml_data ) {
-    foreach( $data as $key => $value ) {
-        if( is_array($value) ) {
-            if( is_numeric($key) ){
-                $key = 'item'.$key; //dealing with <0/>..<n/> issues
-            }
-            $subnode = $xml_data->addChild($key);
-            array_to_xml($value, $subnode);
-        } else {
-            $xml_data->addChild("$key",htmlspecialchars("$value"));
-        }
+    foreach( $data as $dt ) {
+    	foreach ($dt as $key => $value) {
+	        if( is_array($value) ) {
+	            if( is_numeric($key) ){
+	                $key = 'item'.$key; //dealing with <0/>..<n/> issues
+	            }
+	            $subnode = $xml_data->addChild($key);
+	            array_to_xml($value, $subnode);
+	        } else {
+	            $xml_data->addChild("$key",htmlspecialchars("$value"));
+	        }
+	    }
      }
 }
 ?>
