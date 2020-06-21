7b6c5d71d4df4f6e2ca6930707d717c646ef93b6
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 20:54:12 +0530

    sitemap.xml

--- sitemap.php
+++ sitemap.php
@@ -8,18 +8,20 @@ to_xml($xml, $or);
 print $xml->asXML();
 
 function to_xml(SimpleXMLElement $object, array $data) {   
-    foreach ($data as $key => $value) {
-        if (is_array($value)) {
-            $new_object = $object->addChild($key);
-            to_xml($new_object, $value);
-        } else {
-            // if the key is an integer, it needs text with it to actually work.
-            if ($key == (int) $key) {
-                $key = "key_$key";
-            }
+    foreach ($data as $dt) {
+    	foreach ($dt as $key => $value) {
+	        if (is_array($value)) {
+	            $new_object = $object->addChild($key);
+	            to_xml($new_object, $value);
+	        } else {
+	            // if the key is an integer, it needs text with it to actually work.
+	            if ($key == (int) $key) {
+	                $key = "key_$key";
+	            }
 
-            $object->addChild($key, $value);
-        }   
+	            $object->addChild($key, $value);
+	        }
+	    }   
     }   
 }   
 ?>
