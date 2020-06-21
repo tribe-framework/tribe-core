e2d25fee040f9679b564bd100ef47d720d9aa0b7
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 20:41:44 +0530

    sitemap.xml

--- sitemap.php
+++ sitemap.php
@@ -4,6 +4,20 @@
 $or=array();
 $or[0]['url']=array(BASE_URL => 'loc', '2020-06-20' => 'lastmod', '1' => 'priority');
 $xml = new SimpleXMLElement('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
-array_walk_recursive($or, array ($xml, 'addChild'));
+array_to_xml($or, $xml);
 print $xml->asXML();
+
+function array_to_xml( $data, &$xml_data ) {
+    foreach( $data as $key => $value ) {
+        if( is_array($value) ) {
+            if( is_numeric($key) ){
+                $key = 'item'.$key; //dealing with <0/>..<n/> issues
+            }
+            $subnode = $xml_data->addChild($key);
+            array_to_xml($value, $subnode);
+        } else {
+            $xml_data->addChild("$key",htmlspecialchars("$value"));
+        }
+     }
+}
 ?>
