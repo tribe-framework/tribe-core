cd0ac358e679ce64ffc95e51c6243e78f1e3bae9
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 20:59:12 +0530

    sitemap.xml

--- sitemap.php
+++ sitemap.php
@@ -6,20 +6,16 @@ $xml = new SimpleXMLElement('<urlset xmlns="http://www.sitemaps.org/schemas/site
 $or['url']=array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1');
 to_xml($xml, $or); print $xml->asXML();
 
-function to_xml(SimpleXMLElement $object, array $data)
-{   
+function to_xml(SimpleXMLElement $object, array $data) {
     foreach ($data as $key => $value) {
         if (is_array($value)) {
             $new_object = $object->addChild($key);
             to_xml($new_object, $value);
         } else {
-            // if the key is an integer, it needs text with it to actually work.
-            if ($key == (int) $key) {
+            if (is_int($key))
                 $key = "key_$key";
-            }
-
             $object->addChild($key, $value);
-        }   
-    }   
-}   
+        }
+    }
+}
 ?>
