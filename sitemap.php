205a9f129181bc374a936f6d6fc40e827dd72044
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 20:57:20 +0530

    sitemap.xml

--- sitemap.php
+++ sitemap.php
@@ -3,25 +3,23 @@
 <?php
 $or=array();
 $xml = new SimpleXMLElement('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
-$or[0]['url']=array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1');
-to_xml($xml, $or);
-print $xml->asXML();
+$or['url']=array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1');
+to_xml($xml, $or); print $xml->asXML();
 
-function to_xml(SimpleXMLElement $xml, array $data) {   
-    foreach ($data as $dt) {
-    	foreach ($dt as $key => $value) {
-	        if (is_array($value)) {
-	            $xml = $xml->addChild($key);
-	            to_xml($xml, $value);
-	        } else {
-	            // if the key is an integer, it needs text with it to actually work.
-	            if ($key == (int) $key) {
-	                $key = "key_$key";
-	            }
+function to_xml(SimpleXMLElement $object, array $data)
+{   
+    foreach ($data as $key => $value) {
+        if (is_array($value)) {
+            $new_object = $object->addChild($key);
+            to_xml($new_object, $value);
+        } else {
+            // if the key is an integer, it needs text with it to actually work.
+            if ($key == (int) $key) {
+                $key = "key_$key";
+            }
 
-	            $xml->addChild($key, $value);
-	        }
-	    }   
+            $object->addChild($key, $value);
+        }   
     }   
 }   
 ?>
