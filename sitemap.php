59acb947141e7408f8f3cf0bd6af98b960c10bb9
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 20:32:23 +0530

    sitemap.xml

--- sitemap.php
+++ sitemap.php
@@ -2,7 +2,7 @@
 <?php header('Content-type: application/xml; charset=utf-8'); ?>
 <?php
 $or=array (BASE_URL => 'loc', '2020-06-20' => 'lastmod', '1' => 'priority');
-$xml = new SimpleXMLElement('<url/>');
+$xml = new SimpleXMLElement('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset><url/>');
 array_walk_recursive($or, array ($xml, 'addChild'));
 print $xml->asXML();
 ?>
