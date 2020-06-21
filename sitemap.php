0857a61ab19251b2f83f21ac6dc647f409c48625
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 22:20:12 +0530

    sitemap.xml

--- sitemap.php
+++ sitemap.php
@@ -3,11 +3,18 @@
 <?php
 $or=array();
 $xml = new SimpleXMLElement('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
+
 $or['url']=array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1');
 to_xml($xml, $or);
-$or['url']=array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1');
-to_xml($xml, $or);
-print $xml->asXML();
+
+foreach ($types['webapp']['searchable_types'] as $type) {
+	$ids=$dash->get_all_ids($type);
+	foreach ($ids as $idr) {
+		$post=$dash->get_content($idr['id']);
+		$or['url']=array('loc'=>BASE_URL.'/'.$post['type'].'/'.$post['slug'], 'lastmod'=>date('Y-m-d', $post['updated_on']), 'priority'=>'0.7');
+		to_xml($xml, $or);
+	}
+}
 
 function to_xml(SimpleXMLElement $object, array $data) {
     foreach ($data as $key => $value) {
