93de04ec1b8194c7702f266b8b9112eb5f263355
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 20:23:56 +0530

    sitemap.xml

--- sitemap.php
+++ sitemap.php
@@ -1,6 +1,6 @@
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <?php include_once ('config-init.php'); ?>
 <?php header('Content-type: application/xml; charset=utf-8'); ?>
-<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
 <?php
 $or=array (BASE_URL => 'loc', '2020-06-20' => 'lastmod', '1' => 'priority');
