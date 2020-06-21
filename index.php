b6eb28520ec1af206d0fcffe70eec55422beebfd
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-21 13:17:50 +0530

    404 handling in theme

--- index.php
+++ index.php
@@ -7,39 +7,49 @@ if ($_GET['q']) {
 else if ($type && $slug) {
 	$typedata=$types[$type];
 	$postdata=$dash::get_content(array('type'=>$type, 'slug'=>$slug));
-	$postdata_modified=$postdata;
 
-	$append_phrase='';
-	if ($types[$type]['headmeta_title_append']) {
-		foreach ($types[$type]['headmeta_title_append'] as $appendit) {
-			$key=$appendit['type']; $value=$appendit['slug'];
-			$append_phrase.=' '.$types[$type]['headmeta_title_glue'].' '.$types[$key][$value];
-		}
-	}
-	$prepend_phrase='';
-	if ($types[$type]['headmeta_title_prepend']) {
-		foreach ($types[$type]['headmeta_title_prepend'] as $prependit) {
-			$key=$prependit['type']; $value=$prependit['slug'];
-			$prepend_phrase.=$types[$key][$value].' '.$types[$type]['headmeta_title_glue'].' ';
-		}
-	}
-	$postdata_modified[$headmeta_title]=$prepend_phrase.$postdata[$headmeta_title].$append_phrase;
+	if (!$postdata)
+		include_once (THEME_PATH.'/404.php');
+	else {
+		$postdata_modified=$postdata;
 
-	if (file_exists(THEME_PATH.'/single-'.$postdata['id'].'.php'))
-		include_once (THEME_PATH.'/single-'.$postdata['id'].'.php');
-	else if (file_exists(THEME_PATH.'/single-'.$type.'.php'))
-		include_once (THEME_PATH.'/single-'.$type.'.php');
-	else
-		include_once (THEME_PATH.'/single.php');
+		$append_phrase='';
+		if ($types[$type]['headmeta_title_append']) {
+			foreach ($types[$type]['headmeta_title_append'] as $appendit) {
+				$key=$appendit['type']; $value=$appendit['slug'];
+				$append_phrase.=' '.$types[$type]['headmeta_title_glue'].' '.$types[$key][$value];
+			}
+		}
+		$prepend_phrase='';
+		if ($types[$type]['headmeta_title_prepend']) {
+			foreach ($types[$type]['headmeta_title_prepend'] as $prependit) {
+				$key=$prependit['type']; $value=$prependit['slug'];
+				$prepend_phrase.=$types[$key][$value].' '.$types[$type]['headmeta_title_glue'].' ';
+			}
+		}
+		$postdata_modified[$headmeta_title]=$prepend_phrase.$postdata[$headmeta_title].$append_phrase;
+
+		if (file_exists(THEME_PATH.'/single-'.$postdata['id'].'.php'))
+			include_once (THEME_PATH.'/single-'.$postdata['id'].'.php');
+		else if (file_exists(THEME_PATH.'/single-'.$type.'.php'))
+			include_once (THEME_PATH.'/single-'.$type.'.php');
+		else
+			include_once (THEME_PATH.'/single.php');
+	}
 }
 elseif ($type && !$slug) {
 	$typedata=$types[$type];
-	$postids=$dash::get_all_ids($type);
+	
+	if (!$typedata)
+		include_once (THEME_PATH.'/404.php');
+	else {
+		$postids=$dash::get_all_ids($type);
 
-	if (file_exists(THEME_PATH.'/archive-'.$type.'.php'))
-		include_once (THEME_PATH.'/archive-'.$type.'.php');
-	else
-		include_once (THEME_PATH.'/archive.php');
+		if (file_exists(THEME_PATH.'/archive-'.$type.'.php'))
+			include_once (THEME_PATH.'/archive-'.$type.'.php');
+		else
+			include_once (THEME_PATH.'/archive.php');
+	}
 }
 else {
 	include_once (THEME_PATH.'/index.php');
