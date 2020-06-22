a47ff5e8877d67b9af221e73c87a5ad44b5f3d0a
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-22 23:42:04 +0530

    folder auth renamed user

---
+++ user/footer.php
@@ -0,0 +1,7 @@
+<?php
+if (file_exists(THEME_PATH.'/user/footer.php')):
+	include_once (THEME_PATH.'/user/footer.php');
+else: ?>
+</body>
+</html>
+<?php endif; ?>
---
+++ user/profile.php
@@ -0,0 +1,7 @@
+<?php
+include_once ('../config-init.php');
+include_once (ABSOLUTE_PATH.'/auth/header.php');
+?>
+
+
+<?php include_once (ABSOLUTE_PATH.'/auth/footer.php'); ?>
