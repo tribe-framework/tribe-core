0a1c1bcda891c6fc0551c69cea67b48e4ce56064
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-23 11:26:29 +0530

    pdf and excel download icons

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -30,10 +30,14 @@ $( document ).ready(function() {
 		"dom": '<"top"ifl>rt<"bottom"Bp>',
 		"pageLength":50,
 		"order": [[ 0, "desc" ]],
-        "buttons": [
-        	{"extend": 'excel', "text": '<span class="fas fa-file-excel"></span>&nbsp;XLSX', "title": 'data'},
-        	{"extend": 'pdf', "text": '<span class="fas fa-file-pdf"></span>&nbsp;PDF', "title": 'data'},
-        ]
+        "buttons": [{
+            extend: 'collection',
+            text: '<span class="fas fa-file-export"></span>&nbsp;&nbsp;Export data',
+            buttons: [
+	        	{"extend": 'excel', "text": '<span class="fas fa-file-excel"></span>&nbsp;&nbsp;.xlsx', "title": 'data'},
+	        	{"extend": 'pdf', "text": '<span class="fas fa-file-pdf"></span>&nbsp;&nbsp;.pdf', "title": 'data'}
+        	]
+        }]
 	});
 
 	var sli=0;
