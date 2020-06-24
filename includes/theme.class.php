5d45cd0b356f2cf32c39bd9fb7b0a6033c836222
akshay / jigmet <akshay.wildfire@pm.me>
2020-06-24 16:17:57 +0530

    new syntax of isset

--- includes/theme.class.php
+++ includes/theme.class.php
@@ -34,20 +34,20 @@ class theme {
 		if ($items) {
 			$op.='
 			<nav class="navbar '.$css_classes['navbar'].'">
-				<a class="navbar-brand" href="'.$items['logo']['href'] ?? '#'.'" title="'.isset_var($items['logo']['title']).'">'.(isset($items['logo']['src']) && trim($items['logo']['src'])?'<img '.(isset($items['logo']['height'])?'height="'.$items['logo']['height'].'"':'').' src="'.$items['logo']['src'].'">':(isset($items['logo']['name'])?$items['logo']['name'].(isset($items['logo']['byline'])?'<span class="small byline">'.$items['logo']['byline'].'</span>':''):'Wildfire')).'</a>
-				<button class="'.isset_var($css_classes['toggler']).'" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
+				<a class="navbar-brand" href="'.$items['logo']['href']??'#'.'" title="'.$items['logo']['title']??BASE_URL.' logo'.'">'.(isset($items['logo']['src']) && trim($items['logo']['src'])?'<img '.(isset($items['logo']['height'])?'height="'.$items['logo']['height'].'"':'').' src="'.$items['logo']['src'].'">':(isset($items['logo']['name'])?$items['logo']['name'].(isset($items['logo']['byline'])?'<span class="small byline">'.$items['logo']['byline'].'</span>':''):'Wildfire')).'</a>
+				<button class="'.$css_classes['toggler']??''.'" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
 					'.$hamburger_bars.'
 				</button>
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="'.$css_classes['ul'].'">';
-					if (isset_var($items['menu'])) {
+					if (isset($items['menu'])) {
 						foreach ($items['menu'] as $item) {
-							if (is_array(isset_var($item['submenu']))) {
-								$op.='<li class="'.isset_var($css_classes['li']).' dropdown"><a class="'.$css_classes['a'].' dropdown-toggle" href="#" title="'.$item['title'].'" role="button" data-toggle="dropdown">'.$item['name'].'
-									</a><div class="dropdown-menu '.isset_var($css_classes['dropdown']).' '.isset_var($item['dropdown_class']).'">';
-								if (isset_var($item['submenu'])) {
+							if (is_array($item['submenu']??'')) {
+								$op.='<li class="'.$css_classes['li']??''.' dropdown"><a class="'.$css_classes['a']??''.' dropdown-toggle" href="#" title="'.$item['title']??''.'" role="button" data-toggle="dropdown">'.$item['name']??''.'
+									</a><div class="dropdown-menu '.$css_classes['dropdown']??''.' '.$item['dropdown_class']??''.'">';
+								if (isset($item['submenu'])) {
 									foreach ($item['submenu'] as $subitem)
-										$op.='<a class="dropdown-item" href="'.$subitem['href'].'" title="'.($subitem['title']?$subitem['title']:'').'">'.$subitem['name'].'</a>';
+										$op.='<a class="dropdown-item" href="'.$subitem['href']??''.'" title="'.$subitem['title']??''.'">'.$subitem['name']??''.'</a>';
 								}
 								$op.='</div></li>';
 							}
