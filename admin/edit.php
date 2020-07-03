91c4684817ca87dae5896478e993d8a48ace55c7
akshay / jigmet <akshay.wildfire@pm.me>
2020-07-03 10:22:22 +0530

    multi_drop

--- admin/edit.php
+++ admin/edit.php
@@ -263,7 +263,7 @@ if (($_GET['id'] && $post['type']==$type) || !$_GET['id']):
 		<?php endif; ?>
 
 		<?php if ($module_input_type=='multi_drop'): ?>
-		<div class="clearfix form-group mt-5"><?php echo ($module_input_placeholder?$module_input_placeholder:'Select '.$module_input_slug_lang); ?><br><div class="float-left multi_drop w-50"><table class="bg-light table"></table></div><div class="float-left multi_drop w-50"><table class="table packery-table"><thead><th>Options</th></thead><tbody>
+		<div class="clearfix form-group mt-5"><?php echo ($module_input_placeholder?$module_input_placeholder:'Select '.$module_input_slug_lang); ?><br><table class="bg-light table float-left multi_drop w-50"></table><div class="float-left multi_drop w-50"><table class="table packery-table"><thead><th>Options</th></thead><tbody>
 			<?php 
 			if ($options=$module_input_options) {
 				$i=0;
