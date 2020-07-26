8688ed343c462fdfdcaf0155dedca1a54eb2a85f
akshay / jigmet <akshay.wildfire@pm.me>
2020-07-26 09:42:19 +0530

    include captcha class

--- includes/captcha.class.php
+++ includes/captcha.class.php
@@ -3,17 +3,17 @@ class captcha {
 	public $errormsg;
 	public $successmsg;
 	
-	public $left_arr=array();
-	
 	function __construct() {
-		$this->left_arr=array_map('trim', explode(',', CAPTCHA_WORDS));
+
 	}
 
 	function check_captcha ($captcha_input, $captcha_code) {
-		global $sql;
 		$captcha_input = strtolower(preg_replace('/\s+/', '', $captcha_input));
 		$captcha_secret=$this->get_captcha_string_from_code($captcha_code, 0);
-		$sql->executeSQL("DELETE FROM `captcha` WHERE `code`='$captcha_code'");
+
+		unset(CAPTCHA_STRING);
+		unset(CAPTCHA_CODE);
+
 		if ($captcha_input==$captcha_secret)
 			return 1;
 		else
@@ -21,29 +21,25 @@ class captcha {
 	}
 	
 	function show_input_fields ($css_classes='form-control', $css_id='') {
-		global $sql;
-		$string = rand(10000,99999);
-		$code = uniqid();
-		$sql->executeSQL("INSERT INTO `captcha` (`string`, `code`) VALUES ('$string', '$code')");
-		if ($sql->lastInsertID()) {
-			$op = '<div class="form-group col-6"><label for="captcha"><img src="/captcha.php?captcha_code='.$code.'"></label>
+		unset(CAPTCHA_STRING);
+		unset(CAPTCHA_CODE);
+		define('CAPTCHA_STRING', rand(10000,99999));
+		define('CAPTCHA_CODE', uniqid());
+
+		$op = '
+		<div class="form-group col-6"><label for="captcha"><img src="/captcha.php?captcha_code='.CAPTCHA_CODE.'"></label>
 		    <input type="text" name="captcha_input" id="captcha" class="captcha_input form-control" aria-describedby="captchaHelp" placeholder="Type the above digits here">
 		    <small id="emailHelp" class="form-text text-muted">Enter the digits in the CAPTCHA image above.</small>
-		    <input type="hidden" class="captcha_code" name="captcha_code" value="'.$code.'"></div><div class="col"></div>';
-		}
-		else {
-			$op = '<div class="col-6">ERROR: please report to the system administrator.</div><div class="col"></div>';
-		}
+		    <input type="hidden" class="captcha_code" name="captcha_code" value="'.CAPTCHA_CODE.'"></div><div class="col">
+	    </div>';
+		
 		return $op;
 	}
 
-	function get_captcha_string_from_code($captcha_code, $display=1) {
-		global $sql;
-		$q=$sql->executeSQL("SELECT `string` FROM `captcha` WHERE `code`='$captcha_code'
-			");
+	function get_captcha_string_from_code ($captcha_code, $display=1) {
 		if ($display)
-			return $q[0]['string'];
+			return CAPTCHA_STRING;
 		else
-			return strtolower(preg_replace('/\s+/', '', $q[0]['string']));
+			return strtolower(preg_replace('/\s+/', '', CAPTCHA_STRING));
 	}
 }
