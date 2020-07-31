4c2d6652404b5ca3ea82e1851e7816a099790aae
akshay / jigmet <akshay.wildfire@pm.me>
2020-07-31 11:37:22 +0530

    registration user types

--- user/register.php
+++ user/register.php
@@ -3,5 +3,11 @@ include_once ('../config-init.php');
 include_once (ABSOLUTE_PATH.'/auth/header.php');
 ?>
 
+<?php
+foreach ($types as $key => $value) {
+	if ($types[$key]['type']=='content')
+    	echo '<button type="button" class="btn btn-primary btn-lg">'.ucfirst($types[$key]['name']).'</button>';
+}
+?>
 
 <?php include_once (ABSOLUTE_PATH.'/auth/footer.php'); ?>
