14443b9b781624b5f3da5dd1771f23c996fa8075
akshay / jigmet <akshay.wildfire@pm.me>
2020-08-01 23:23:35 +0530

    THEME_URL as javascript variable

--- themes/wildfire-2020/footer.php
+++ themes/wildfire-2020/footer.php
@@ -5,7 +5,6 @@
 	<script src="/plugins/bootstrap/dist/js/bootstrap.min.js"></script>
 	<script src="https://unpkg.com/flickity@2/dist/flickity.pkgd.min.js"></script>
 
-	<script type="text/javascript">var THEME_URL = "<?php echo THEME_URL; ?>";</script>
 	<script src="<?php echo THEME_URL; ?>/js/custom.js?v=<?php echo time(); ?>"></script>
 </body>
 </html>
