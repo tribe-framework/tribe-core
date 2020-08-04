34060afa3a61a5bc8b32da6416538996936ae889
akshay / jigmet <akshay.wildfire@pm.me>
2020-08-04 08:49:02 +0530

    user theme header and footer

--- user/footer.php
+++ user/footer.php
@@ -1,6 +1,6 @@
 <?php
-if (file_exists(THEME_PATH.'/user/footer.php')):
-	include_once (THEME_PATH.'/user/footer.php');
+if (($types['webapp']['dashboard_theme']??false) && file_exists(THEME_PATH.'/footer.php'))
+	include_once (THEME_PATH.'/footer.php');
 else: ?>
 </body>
 </html>
