c55316a3bb22a530553ae7a8cd25bcd0a25e717d
akshay / wildfire <akshay.wildfire@pm.me>
2020-08-11 15:15:16 +0530

    api integration basic

---
+++ api/push-meta.php
@@ -0,0 +1,15 @@
+<?php
+include_once ('../init.php');
+if ($_POST['WEBAPP_API_KEY']==WEBAPP_API_KEY) {
+	if (!$_POST['content_privacy'])
+		$_POST['content_privacy']='public';
+	$or=array();
+	if ($_POST['id']) {
+		$or['id']=$_POST['id'];
+		$dash->push_content_meta($_POST['id'], $_POST['meta_key'], $_POST['meta_value']);
+	}
+	echo json_encode($or);
+}
+else
+	echo 'Not allowed.';
+?>
---
+++ api/push.php
@@ -0,0 +1,12 @@
+<?php
+include_once ('../init.php');
+if ($_POST['WEBAPP_API_KEY']==WEBAPP_API_KEY) {
+	if (!$_POST['content_privacy'])
+		$_POST['content_privacy']='public';
+	$or=array();
+	$or['id']=$dash->push_content($_POST);
+	echo json_encode($or);
+}
+else
+	echo 'Not allowed.';
+?>
