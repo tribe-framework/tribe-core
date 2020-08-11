74e0c8859c0edb2fac769197354ee507aac09c38
akshay / wildfire <akshay.wildfire@pm.me>
2020-08-11 15:22:34 +0530

    test

--- api/push-meta.php
+++ api/push-meta.php
@@ -1,13 +1,13 @@
 <?php
 header('Content-Type: application/json');
 include_once ('../init.php');
-if ($_POST['WEBAPP_API_KEY']==WEBAPP_API_KEY) {
-	if (!$_POST['content_privacy'])
-		$_POST['content_privacy']='public';
+if ($_GET['WEBAPP_API_KEY']==WEBAPP_API_KEY) {
+	if (!$_GET['content_privacy'])
+		$_GET['content_privacy']='public';
 	$or=array();
-	if ($_POST['id']) {
-		$or['id']=$_POST['id'];
-		$dash->push_content_meta($_POST['id'], $_POST['meta_key'], $_POST['meta_value']);
+	if ($_GET['id']) {
+		$or['id']=$_GET['id'];
+		$dash->push_content_meta($_GET['id'], $_GET['meta_key'], $_GET['meta_value']);
 	}
 	echo json_encode($or);
 }
--- api/push.php
+++ api/push.php
@@ -1,11 +1,11 @@
 <?php
 header('Content-Type: application/json');
 include_once ('../init.php');
-if ($_POST['WEBAPP_API_KEY']==WEBAPP_API_KEY) {
-	if (!$_POST['content_privacy'])
-		$_POST['content_privacy']='public';
+if ($_GET['WEBAPP_API_KEY']==WEBAPP_API_KEY) {
+	if (!$_GET['content_privacy'])
+		$_GET['content_privacy']='public';
 	$or=array();
-	$or['id']=$dash->push_content($_POST);
+	$or['id']=$dash->push_content($_GET);
 	echo json_encode($or);
 }
 else
