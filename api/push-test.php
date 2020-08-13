b6b48bb08a03528985ac0b046a8f3bfa383f319b
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-08-13 13:10:49 +0530

    updated config and enabled json parsing

--- api/push-test.php
+++ api/push-test.php
@@ -1,5 +1,10 @@
 <?php
-//header('Content-Type: application/json');
+$json = file_get_contents('php://input');
+$data = json_decode($json);
+
 var_dump($_GET);
 var_dump($_POST);
+
+var_dump($json);
+var_dump($data);
 ?>
--- config/vars.php
+++ config/vars.php
@@ -1,16 +1,16 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 date_default_timezone_set('Asia/Kolkata');
-define('BASE_URL', 'https://xyz.com');
-define('BARE_URL', 'xyz.com');
-define('ABSOLUTE_PATH', '/var/www/html/xyz.com');
-define('THEME_URL', 'https://xyz.com/themes/xyz.com');
-define('THEME_PATH', '/var/www/html/xyz.com/themes/xyz.com');
-define('DB_NAME', 'xyz_com');
-define('DB_USER', 'xyz_com');
-define('DB_PASS', 'xyz_pass');
+define('BASE_URL', 'https://localhost');
+define('BARE_URL', 'localhost');
+define('ABSOLUTE_PATH', '/home/apurv/srv/html/prism.wf');
+define('THEME_URL', 'https://localhost/themes/wildfire-2020');
+define('THEME_PATH', '/home/apurv/srv/html/prism.wf/themes/wildfire-2020');
+define('DB_NAME', 'prism');
+define('DB_USER', 'root');
+define('DB_PASS', 'root');
 define('DB_HOST', 'localhost');
 ?>
