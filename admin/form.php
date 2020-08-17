0177084774fb9956960abd025f806c28fe1819f0
akshay / jigmet <akshay.wildfire@pm.me>
2020-08-17 23:45:53 +0530

    checkbox

--- admin/form.php
+++ admin/form.php
@@ -188,7 +188,7 @@
 		<?php if ($module_input_type=='checkbox'): ?>
 		<div class="input-group mt-5">
 			<div class="custom-control custom-checkbox">
-			  <input type="checkbox" name="<?php echo $module_input_slug_lang; ?>" class="custom-control-input" id="customCheck_<?php echo $module_input_slug_lang; ?>" value="<?php echo ($post[$module_input_slug_lang]?$post[$module_input_slug_lang]:$module_input_default_value); ?>">
+			  <input type="checkbox" name="<?php echo $module_input_slug_lang; ?>" class="custom-control-input" id="customCheck_<?php echo $module_input_slug_lang; ?>" value="1" <?php echo ($post[$module_input_slug_lang]?'checked="checked"':''); ?>>
 			  <label class="custom-control-label" for="customCheck_<?php echo $module_input_slug_lang; ?>"><?php echo ($module_input_placeholder??''); ?></label>
 			</div>
 		</div>
