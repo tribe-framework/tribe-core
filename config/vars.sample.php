27626e62331e8fa40f34f1185858fe356ac7d0b7
akshay / wildfire <akshay.wildfire@pm.me>
2020-08-18 14:27:23 +0530

    var name adjustment

--- config/vars.sample.php
+++ config/vars.sample.php
@@ -4,13 +4,13 @@
 //error_reporting(E_ALL);
 error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 date_default_timezone_set('Asia/Kolkata');
-define('BASE_URL', 'https://xyz.com');
-define('BARE_URL', 'xyz.com');
-define('ABSOLUTE_PATH', '/var/www/html/xyz.com');
-define('THEME_URL', 'https://xyz.com/themes/xyz.com');
-define('THEME_PATH', '/var/www/html/xyz.com/themes/xyz.com');
-define('DB_NAME', 'xyz_com');
-define('DB_USER', 'xyz_com');
-define('DB_PASS', 'xyz_pass');
+define('BASE_URL', 'https://xyz-domain-var');
+define('BARE_URL', 'xyz-domain-var');
+define('ABSOLUTE_PATH', '/var/www/html/xyz-domain-var');
+define('THEME_URL', 'https://xyz-domain-var/themes/xyz-domain-var');
+define('THEME_PATH', '/var/www/html/xyz-domain-var/themes/xyz-domain-var');
+define('DB_NAME', 'xyz-db-name-var');
+define('DB_USER', 'xyz-db-name-var');
+define('DB_PASS', 'xyz-db-pass-var');
 define('DB_HOST', 'localhost');
 ?>
