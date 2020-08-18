d24531cc50fdd018a497e7e7539b20cfe65f6382
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-08-18 17:24:17 +0530

    elegantly handle non-authorised connection
    added api call for reverse-ip location gathering

--- api/push-meta.php
+++ api/push-meta.php
@@ -5,20 +5,21 @@ $data = json_decode($json);
 header('Content-Type: application/json');
 include_once ('../init.php');
 
-if ($data->WEBAPP_API_KEY) {
-	if (!$data->content_privacy) {
-		$data->content_privacy='public';
-	}
-
-	$or=array();
-
-	if ($data->id) {
-		$or['id']=$data->id;
-		$dash->push_content_meta($data->id, $data->meta_key, $data->meta_value);
-	}
-
-	echo json_encode($or);
-} else {
-	echo json_encode(array('error'=>'Not allowed.'));
+// if the client isn't authorised for server, end the connection
+if ($data->WEBAPP_API_KEY == WEBAPP_API_KEY) {
+	die(json_encode(array('error'=>'Not allowed.')));
 }
+
+if (!$data->content_privacy) {
+	$data->content_privacy='public';
+}
+
+$or=array();
+
+if ($data->id) {
+	$or['id']=$data->id;
+	$dash->push_content_meta($data->id, $data->meta_key, $data->meta_value);
+}
+
+echo json_encode($or);
 ?>
