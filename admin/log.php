c4af30787344055051b905e5740a54b33b5a319e
akshay / wildfire <akshay.wildfire@pm.me>
2020-08-19 11:59:18 +0530

    log file

--- admin/log.php
+++ admin/log.php
@@ -1,5 +1,5 @@
 <?php
 include_once ('../init.php');
 include_once (ABSOLUTE_PATH.'/admin/header.php');
-
+var_dump($dash->do_shell_command('tail -200 /var/log/apache2/error.log'));
 include_once (ABSOLUTE_PATH.'/admin/footer.php'); ?>
