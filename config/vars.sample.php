c6dbf3bb43631be6ab8cd9b8c950a72721fb1d8e
akshay / wildfire <akshay.wildfire@pm.me>
2020-08-19 08:14:13 +0530

    vars files streamline

--- config/vars.sample.php
+++ config/vars.sample.php
@@ -1,9 +1,4 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
-date_default_timezone_set('Asia/Kolkata');
 define('BASE_URL', 'https://xyz-domain-var');
 define('BARE_URL', 'xyz-domain-var');
 define('ABSOLUTE_PATH', '/var/www/html/xyz-domain-var');
--- themes/wildfire-2020/config/vars.php
+++ themes/wildfire-2020/config/vars.php
@@ -2,12 +2,22 @@
 //ini_set('display_errors', 1);
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
+error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+
+//timezone
+date_default_timezone_set('Asia/Kolkata');
+
+//allow upload in file types
 define('UPLOAD_FILE_TYPES', '/\.(zip|png|jpe?g|gif|pdf|doc|docx|xls|xlsx|mov|mp4|vtt)$/i');
+
+//S3 backup credentials for /config/backup.php
 define('S3_BKUP_HOST_BASE', 's3.wasabisys.com');
 define('S3_BKUP_HOST_BUCKET', '%(bucket)s.s3.wasabisys.com');
 define('S3_BKUP_ACCESS_KEY', '');
 define('S3_BKUP_SECRET_KEY', '');
 define('S3_BKUP_FOLDER_NAME', BARE_URL);
+
+//other services
 define('GOOGLE_MAP_API_KEY_1', '');
 define('GOOGLE_MAP_API_KEY_2', '');
 define('YOUTUBE_CLIENT_ID', '');
@@ -20,6 +30,8 @@ define('CLOUDFLARE_ACCOUNT_ID', '');
 define('CLOUDFLARE_ACCOUNT_KEY', '');
 define('CLOUDNS_AUTH_ID', '');
 define('CLOUDNS_AUTH_PASSWORD', '');
+
+//payment gateways
 define('INSTAMOJO_KEY', '');
 define('INSTAMOJO_TOKEN', '');
 define('INSTAMOJO_SALT', '');
