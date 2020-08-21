ce0bceb324b6ba6b9743f03debfc5a1db3e4ae8e
akshay / wildfire <akshay.wildfire@pm.me>
2020-08-21 09:45:15 +0530

    user dashboard access

--- user/index.php
+++ user/index.php
@@ -2,6 +2,8 @@
 include_once ('../init.php');
 include_once (ABSOLUTE_PATH.'/user/header.php');
 
+if (!$_SESSION['user_id']) {header ('Location: /user/login'); die();}
+
 if (($types['webapp']['user_theme']??false) && file_exists(THEME_PATH.'/user-index.php')):
 	
 	include_once (THEME_PATH.'/user-index.php');
