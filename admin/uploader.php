8c9396e74006bff96df256ce15a77592db9a9792
akshay / jigmet <akshay.wildfire@pm.me>
2020-09-01 20:31:43 +0530

    blueimp shifted to plugins

--- admin/uploader.php
+++ admin/uploader.php
@@ -1,17 +1,10 @@
 <?php
-/*
- * jQuery File Upload Plugin PHP Example
- * https://github.com/blueimp/jQuery-File-Upload
- *
- * Copyright 2010, Sebastian Tschan
- * https://blueimp.net
- *
- * Licensed under the MIT license:
- * https://opensource.org/licenses/MIT
- */
-
 error_reporting(E_ALL | E_STRICT);
 include_once ('../init.php');
+
+//https://github.com/blueimp/jQuery-File-Upload/blob/master/server/php/UploadHandler.php
+include_once(ABSOLUTE_PATH.'/plugins/blueimp-jquery-file-upload/UploadHandler.php');
+
 if (UPLOAD_FILE_TYPES)
 	$upload_handler = new UploadHandler(array('script_url'=>ABSOLUTE_PATH.'/admin/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES));
 else
