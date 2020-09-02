acdee970166b4eea1cf69ed3d78b94c342476837
akshay / wildfire <akshay.wildfire@pm.me>
2020-09-02 18:04:40 +0530

    moved error reporting to init.php - dev and prod

--- init.php
+++ init.php
@@ -4,15 +4,20 @@ session_start();
 
 include_once 'config/vars.php';
 
+if (file_exists(THEME_PATH.'/config/vars.php')) {
+    include_once(THEME_PATH.'/config/vars.php');
+}
+
 // browser debugging
 if (ENV == 'dev') {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 }
-
-if (file_exists(THEME_PATH.'/config/vars.php')) {
-    include_once(THEME_PATH.'/config/vars.php');
+else {
+    ini_set('display_errors', 0);
+    ini_set('display_startup_errors', 0);
+	error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
 }
 
 $session_user = $_SESSION['user'] ?? NULL;
@@ -33,7 +38,7 @@ $theme = new theme();
 
 include_once(THEME_PATH.'/functions.php');
 
-$types=$dash::get_types(THEME_PATH.'/config/types.json');
+$types=$dash->get_types(THEME_PATH.'/config/types.json');
 $menus=json_decode(file_get_contents(THEME_PATH.'/config/menus.json'), true);
 $admin_menus=json_decode(file_get_contents(ABSOLUTE_PATH.'/admin/config/admin_menus.json'), true);
 
@@ -50,6 +55,6 @@ if (isset($_GET['ext'])) { //for theme
         $slug=$dash->do_unslugify($ext[1]);
     }
 } elseif (isset($_GET['type'])) { //for dashboard
-    $type=$dash::do_unslugify($_GET['type']);
+    $type=$dash->do_unslugify($_GET['type']);
 }
 ?>
--- themes/wildfire-2020/config/vars.php
+++ themes/wildfire-2020/config/vars.php
@@ -1,8 +1,6 @@
 <?php
-//ini_set('display_errors', 1);
-//ini_set('display_startup_errors', 1);
-//error_reporting(E_ALL);
-error_reporting(E_ALL & ~E_NOTICE & ~E_WARNING);
+//dev or prod
+define('ENV', 'prod');
 
 //timezone
 date_default_timezone_set('Asia/Kolkata');
