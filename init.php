5bb68a9e4ca858df4d2b7ef6ff7e33692e123fc6
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-09-02 17:32:04 +0530

    updated init.php to line with code-styling

--- init.php
+++ init.php
@@ -6,13 +6,13 @@ include_once 'config/vars.php';
 
 // browser debugging
 if (ENV == 'dev') {
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
 }
 
 if (file_exists(THEME_PATH.'/config/vars.php')) {
-	include_once(THEME_PATH.'/config/vars.php');
+    include_once(THEME_PATH.'/config/vars.php');
 }
 
 $session_user = $_SESSION['user'] ?? NULL;
@@ -25,7 +25,7 @@ $userless_install=0;
 $q=$sql->executeSQL("SELECT `id` FROM `data` WHERE `content`->'$.type'='user'");
 
 if (!$q[0]['id']) {
-	$userless_install=1;
+    $userless_install=1;
 }
 
 $dash = new dash();
@@ -40,16 +40,16 @@ $admin_menus=json_decode(file_get_contents(ABSOLUTE_PATH.'/admin/config/admin_me
 isset($types['webapp']['lang'])?:$types['webapp']['lang']='en';
 
 if (isset($_GET['ext'])) { //for theme
-	$ext=explode('/', $_GET['ext']);
+    $ext=explode('/', $_GET['ext']);
 
-	if (count($ext)) {
-		$type=$dash->do_unslugify($ext[0]);
-	}
+    if (count($ext)) {
+        $type=$dash->do_unslugify($ext[0]);
+    }
 
-	if (count($ext)>1) {
-		$slug=$dash->do_unslugify($ext[1]);
-	}
+    if (count($ext)>1) {
+        $slug=$dash->do_unslugify($ext[1]);
+    }
 } elseif (isset($_GET['type'])) { //for dashboard
-	$type=$dash::do_unslugify($_GET['type']);
+    $type=$dash::do_unslugify($_GET['type']);
 }
 ?>
