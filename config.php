dcc065473bd453c965a967fe4470f3a9b861c24b
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-09-05 13:33:55 +0530

    removed vars.php from config/
    split code for vars.php amongst /vars.php & /config.php

--- config.php
+++ config.php
@@ -1,14 +1,5 @@
 <?php
-define('THEME', 'prism.wf');
-
-# database credentials
-define('DB_NAME', 'prism');
-define('DB_USER', 'root');
-define('DB_PASS', 'root');
-define('DB_HOST', 'localhost');
-
-# set ENV to dev for verbose debug of app
-define('ENV', '');
+require_once 'vars.php';
 
 # do not modify beyond this line if not needed
 define('BARE_URL', $_SERVER['HTTP_HOST']);
@@ -17,8 +8,5 @@ define('ABSOLUTE_PATH', __DIR__);
 define('THEME_URL', BARE_URL . '/themes/' . THEME);
 define('THEME_PATH', ABSOLUTE_PATH . '/themes/' . THEME);
 
-/**
- * initiating the autoloader
- */
-include_once ABSOLUTE_PATH . '/includes/autoloader.inc.php';
+session_save_path('/tmp');
 ?>
