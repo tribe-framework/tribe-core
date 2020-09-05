cfb24a0e7118004c2fc8c49f394836ecbb19a711
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-09-05 13:37:34 +0530

    added new route for prism

---
+++ routes/prism/index.php
@@ -0,0 +1,38 @@
+<?php
+/*session_start();
+
+include_once $_SERVER['DOCUMENT_ROOT'].'/init.php';
+
+$sql = new MySQL();
+
+include_once('trac.class.php');
+$trac = new Trac();
+
+unset($_SERVER['SERVER_SIGNATURE']);
+
+$prism_visit_id=$_POST['prism_visit_id'];
+
+if (!$prism_visit_id) {
+	$prism_visit_id = $trac->push_visit(array_merge($_SERVER, $_POST));
+} elseif ($_POST['action'] == 'click') {
+	$trac->push_visit_meta($prism_visit_id, 'click_'.time(), json_encode($_POST));
+} elseif ($_POST['unload']) {
+	$trac->push_visit_meta($prism_visit_id, 'time_spent', $_POST['time_spent']);
+}
+
+header('Content-Type: application/json');
+echo json_encode(array('prism_visit_id'=>$prism_visit_id));
+*/
+
+// re-working the api
+// $data = new PostBody();
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+	$e = new PageError();
+	$e->notFound();
+}
+
+$data = PostBody::getPost()->print();
+// echo $data->hello;
+
+?>
