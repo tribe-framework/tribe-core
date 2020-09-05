ddcef6de059ca63719b12d514424f4d65d418866
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-09-05 13:37:19 +0530

    added routes for server errors

--- routes/errors/403.php
+++ routes/errors/403.php
@@ -1,6 +1,8 @@
 <?php
 $errorPath = THEME_PATH . '/403.php';
 
+header($_SERVER['SERVER_PROTOCOL'].' 403', true, 403);
+
 if (!file_exists($errorPath)) {
   echo 'Error: 403';
   die();
--- routes/errors/404.php
+++ routes/errors/404.php
@@ -2,6 +2,7 @@
 $errorPath = THEME_PATH . '/404.php';
 
 if (file_exists($errorPath)) {
+  header($_SERVER['SERVER_PROTOCOL'].' 404', true, 404);
   include_once $errorPath;
 }
 ?>
