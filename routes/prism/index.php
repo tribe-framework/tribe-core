27131dbcea75a63bc81626a01fa455c05affb7a9
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-09-09 17:56:49 +0530

    refreshed prism post api

--- routes/prism/index.php
+++ routes/prism/index.php
@@ -1,38 +1,33 @@
 <?php
-/*session_start();
+use WildFire\API;
 
-include_once $_SERVER['DOCUMENT_ROOT'].'/init.php';
-
-$sql = new MySQL();
-
-include_once('trac.class.php');
-$trac = new Trac();
-
-unset($_SERVER['SERVER_SIGNATURE']);
-
-$prism_visit_id=$_POST['prism_visit_id'];
-
-if (!$prism_visit_id) {
-	$prism_visit_id = $trac->push_visit(array_merge($_SERVER, $_POST));
-} elseif ($_POST['action'] == 'click') {
-	$trac->push_visit_meta($prism_visit_id, 'click_'.time(), json_encode($_POST));
-} elseif ($_POST['unload']) {
-	$trac->push_visit_meta($prism_visit_id, 'time_spent', $_POST['time_spent']);
-}
-
-header('Content-Type: application/json');
-echo json_encode(array('prism_visit_id'=>$prism_visit_id));
-*/
-
-// re-working the api
-// $data = new PostBody();
+$sql = new WildFire\MySQL();
+$trac = new WildFire\Trac();
 
 if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-	$e = new PageError();
-	$e->notFound();
+    WildFire\PageError::notFound();
 }
 
-$data = PostBody::getPost()->print();
-// echo $data->hello;
+/**
+ * Server to post request
+ */
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    handlePost();
+}
 
+function handlePost () {
+    unset($_SERVER['SERVER_SIGNATURE']);
+    $body = API::getRequest();
+    $prism_visit_id;
+
+    if (!isset($body->prism_visit_id)) {
+        $prism_visit_id = $trac->push_visit(array_merge($_SERVER, (array) $body));
+    } elseif (($body->action ?? '') == 'click') {
+        $trac->push_visit_meta($prism_visit_id, 'click_'.time(), json_encode($body));
+    } elseif (isset($body->unload)) {
+        $trac->push_visit_meta($prism_visit_id, 'time_spent', $body->time_spent);
+    }
+
+    API::json(array('prism_visit_id'=>$prism_visit_id ?? $body->prism_visit_id));
+}
 ?>
