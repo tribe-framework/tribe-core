0c23a23693beb2db0ff2eb858f05cb32da435ea0
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-09-14 16:42:20 +0530

    added test on $post

--- admin/form.php
+++ admin/form.php
@@ -423,16 +423,18 @@ foreach ($types[$type]['modules'] as $module) {
 		<div class="col-12 p-0 mb-4 d-block">
 			<?php
 			$i=0;
-			foreach ($post[$module_input_slug_lang] as $file) {
-				echo '<p class="mt-2 file done"><span>'.urldecode(basename($file)).'</span>&nbsp;&nbsp;<span class="delete_btn btn btn-sm btn-outline-danger"><span class="fas fa-trash-alt"></span></span><input type="hidden" name="'.$module_input_slug_lang.'[]" value="'.$file.'">&nbsp;&nbsp;<span class="copy_btn btn btn-sm btn-outline-primary" data-clipboard-text="'.$file.'"><span class="fas fa-link"></span>&nbsp;copy URL</span>&nbsp;&nbsp;<a style="display: inline-block;" class="btn btn-sm btn-outline-primary" href="'.$file.'" target="new"><span class="fas fa-external-link-alt"></span>&nbsp;view</a>';
-				if (is_array($module['input_bunching'])) {
-					echo '&nbsp;&nbsp;<select class="btn btn-sm btn-outline-primary" name="'.$module_input_slug_lang.'_bunching[]"><option value="">file option</option>';
-					foreach ($module['input_bunching'] as $opt)
-						echo '<option value="'.$opt['slug'].'" '.(($post[$module_input_slug_lang.'_bunching'][$i]==$opt['slug'])?'selected="selected"':'').'>'.$opt['title'].'</option>';
-					echo '</select>';
+			if (isset($post)) {
+				foreach ($post[$module_input_slug_lang] as $file) {
+					echo '<p class="mt-2 file done"><span>'.urldecode(basename($file)).'</span>&nbsp;&nbsp;<span class="delete_btn btn btn-sm btn-outline-danger"><span class="fas fa-trash-alt"></span></span><input type="hidden" name="'.$module_input_slug_lang.'[]" value="'.$file.'">&nbsp;&nbsp;<span class="copy_btn btn btn-sm btn-outline-primary" data-clipboard-text="'.$file.'"><span class="fas fa-link"></span>&nbsp;copy URL</span>&nbsp;&nbsp;<a style="display: inline-block;" class="btn btn-sm btn-outline-primary" href="'.$file.'" target="new"><span class="fas fa-external-link-alt"></span>&nbsp;view</a>';
+					if (is_array($module['input_bunching'])) {
+						echo '&nbsp;&nbsp;<select class="btn btn-sm btn-outline-primary" name="'.$module_input_slug_lang.'_bunching[]"><option value="">file option</option>';
+						foreach ($module['input_bunching'] as $opt)
+							echo '<option value="'.$opt['slug'].'" '.(($post[$module_input_slug_lang.'_bunching'][$i]==$opt['slug'])?'selected="selected"':'').'>'.$opt['title'].'</option>';
+						echo '</select>';
+					}
+					echo ($module['input_descriptor']?'&nbsp;&nbsp;<button type="button" class="btn btn-sm btn-outline-primary m-1" data-toggle="modal" data-target="#'.$module_input_slug_lang.'_descriptor_m_'.$i.'">descriptor</button><div class="modal fade" id="'.$module_input_slug_lang.'_descriptor_m_'.$i.'" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">add file descriptor</h5><button type="button" class="close" data-dismiss="modal" aria-label="close"><span aria-hidden="true">×</span></button></div><div class="modal-body"><textarea name="'.$module_input_slug_lang.'_descriptor[]" class="form-control" placeholder="enter file descriptor">'.$post[$module_input_slug_lang.'_descriptor'][$i].'</textarea><input name="'.$module_input_slug_lang.'_descriptor_date[]" value="'.$post[$module_input_slug_lang.'_descriptor_date'][$i].'" type="date" class="form-control" placeholder="enter file date"></div><div class="modal-footer"><button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">save</button></div></div></div></div>':'').'</p>';
+					$i++;
 				}
-				echo ($module['input_descriptor']?'&nbsp;&nbsp;<button type="button" class="btn btn-sm btn-outline-primary m-1" data-toggle="modal" data-target="#'.$module_input_slug_lang.'_descriptor_m_'.$i.'">descriptor</button><div class="modal fade" id="'.$module_input_slug_lang.'_descriptor_m_'.$i.'" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><div class="modal-header"><h5 class="modal-title">add file descriptor</h5><button type="button" class="close" data-dismiss="modal" aria-label="close"><span aria-hidden="true">×</span></button></div><div class="modal-body"><textarea name="'.$module_input_slug_lang.'_descriptor[]" class="form-control" placeholder="enter file descriptor">'.$post[$module_input_slug_lang.'_descriptor'][$i].'</textarea><input name="'.$module_input_slug_lang.'_descriptor_date[]" value="'.$post[$module_input_slug_lang.'_descriptor_date'][$i].'" type="date" class="form-control" placeholder="enter file date"></div><div class="modal-footer"><button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">save</button></div></div></div></div>':'').'</p>';
-				$i++;
 			}
 			?>
 		</div>
