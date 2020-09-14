6237733c8ee758f16da97ba5d624152592c1571e
Apurv Jyotirmay <apurvj199512@gmail.com>
2020-09-14 12:18:49 +0530

    cleaned up code and fixed errors

--- admin/footer.php
+++ admin/footer.php
@@ -3,14 +3,24 @@
 		<div class="container my-5">
 		    <div class="row">
 		      <div class="col-md">
-		        <a href="https://wildfirego.com"><img class="w-40" src="/admin/img/logo.png"></a>
-		        <p class="text-muted small mb-3 mt-4 pr-5">Made with <span class="fas fa-heart"></span><?php echo ($types['webapp']['headmeta_title']?'<br><em>for '.$types['webapp']['headmeta_title'].'</em>':''); ?></p><p class="text-muted small my-3 pr-5">Wildfire is a technology consultancy based in New Delhi, India</p><p class="text-muted small my-3 pr-5">&copy; <?php echo (date('Y')=='2020'?date('Y'):'2020 - '.date('Y')); ?></p>
+				<a href="https://wildfirego.com">
+					<img class="w-40" src="/admin/img/logo.png">
+				</a>
+				<p class="text-muted small mb-3 mt-4 pr-5">
+					Made with <span class="fas fa-heart"></span><?= $types['webapp']['headmeta_title']?'<br><em>for '.$types['webapp']['headmeta_title'].'</em>':''; ?>
+				</p>
+				<p class="text-muted small my-3 pr-5">
+					Wildfire is a technology consultancy based in New Delhi, India
+				</p>
+				<p class="text-muted small my-3 pr-5">
+					&copy; <?= date('Y')=='2020' ? date('Y') : '2020 - '.date('Y') ?>
+				</p>
 		      </div>
 		      <div class="col-md">
-		        <?php echo $theme->get_menu($admin_menus['admin_footer_1'], array('ul'=>'list-unstyled mt-5 pt-2 pl-md-5', 'li'=>'', 'a'=>'small')); ?>
+		        <?= $theme->get_menu($admin_menus['admin_footer_1'], array('ul'=>'list-unstyled mt-5 pt-2 pl-md-5', 'li'=>'', 'a'=>'small')); ?>
 		      </div>
 		      <div class="col-md">
-		        <?php echo $theme->get_menu($admin_menus['admin_footer_2'], array('ul'=>'list-unstyled mt-5 pt-2 pl-md-5', 'li'=>'', 'a'=>'small')); ?>
+		        <?= $theme->get_menu($admin_menus['admin_footer_2'], array('ul'=>'list-unstyled mt-5 pt-2 pl-md-5', 'li'=>'', 'a'=>'small')); ?>
 		      </div>
 		    </div>
 		</div>
@@ -24,7 +34,7 @@
 	<script src="/plugins/datatables/datatables.min.js"></script>
 	<script src="/plugins/clipboard.min.js"></script>
 	<script src="/plugins/keymaster.js"></script>
-	<script src="<?php echo BASE_URL; ?>/admin/js/custom.js?v=<?php echo time(); ?>"></script>
+	<script src="<?= BASE_URL; ?>/admin/js/custom.js?v=<?= time(); ?>"></script>
 	<script src="https://unpkg.com/draggabilly@2/dist/draggabilly.pkgd.min.js"></script>
 	<script src="https://unpkg.com/packery@2/dist/packery.pkgd.min.js"></script>
 
@@ -39,11 +49,20 @@
 	<script src="https://blueimp.github.io/jQuery-File-Upload/js/vendor/jquery.ui.widget.js"></script>
 	<script src="https://blueimp.github.io/jQuery-File-Upload/js/jquery.iframe-transport.js"></script>
 	<script src="https://blueimp.github.io/jQuery-File-Upload/js/jquery.fileupload.js"></script>
-    <?php if (!empty(GOOGLE_MAP_API_KEY_1))	echo '<script src="https://maps.googleapis.com/maps/api/js?key='.GOOGLE_MAP_API_KEY_1.'&libraries=places&callback=initMap" async defer></script>'; ?>
+	<?php if (defined('GOOGLE_MAP_API_KEY_1') && !empty(GOOGLE_MAP_API_KEY_1)): ?>
+		<script src="https://maps.googleapis.com/maps/api/js?key='<?= GOOGLE_MAP_API_KEY_1 ?>'&libraries=places&callback=initMap" async defer></script>
+	<?php endif; ?>
 
-	<?php echo (isset($types['webapp']['admin_confetti'])?'<script src="https://cdn.jsdelivr.net/gh/mathusummut/confetti.js/confetti.min.js"></script><script>$(document).on(\'click\', \'.save_btn\', function(e) {confetti.start(1000);});</script>':''); ?>
+	<?php if (isset($types['webapp']['admin_confetti'])): ?>
+		<script src="https://cdn.jsdelivr.net/gh/mathusummut/confetti.js/confetti.min.js"></script>
+		<script>
+			$(document).on('click', '.save_btn', function(e) {
+				confetti.start(1000);
+			});
+		</script>
+	<?php endif; ?>
 
-    <script src="<?php echo BASE_URL; ?>/admin/js/list.js?v=<?php echo time(); ?>"></script>
-    <script src="<?php echo BASE_URL; ?>/admin/js/edit.js?v=<?php echo time(); ?>"></script>
+    <script src="<?= BASE_URL; ?>/admin/js/list.js?v=<?= time() ?>"></script>
+    <script src="<?= BASE_URL; ?>/admin/js/edit.js?v=<?= time() ?>"></script>
 </body>
 </html>
