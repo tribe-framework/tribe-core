7a56203f94961c621b4cec2d72f9934d98bd7a00
akshay / jigmet <akshay.wildfire@pm.me>
2020-09-20 00:36:58 +0530

    Undefined index

--- plugins/prism/trac.php
+++ plugins/prism/trac.php
@@ -15,10 +15,10 @@ $prism_visit_id=$_POST['prism_visit_id'];
 
 if (!$prism_visit_id) {
 	$prism_visit_id = $trac->push_visit(array_merge($_SERVER, $_POST));
-} elseif ($_POST['action'] == 'click') {
+} elseif (isset($_POST['action']) && $_POST['action'] == 'click') {
 	$trac->push_visit_meta($prism_visit_id, 'click_'.time(), json_encode($_POST));
-} elseif ($_POST['unload']) {
-	$trac->push_visit_meta($prism_visit_id, 'time_spent', $_POST['time_spent']);
+} elseif (isset($_POST['unload']) && $_POST['unload']) {
+	$trac->push_visit_meta($prism_visit_id, 'time_spent', ($_POST['time_spent']??false));
 }
 
 header('Content-Type: application/json');
