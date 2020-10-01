bac63d717182edbc0644360003aca0eeb631cf3e
akshay / wildfire <akshay.wildfire@pm.me>
2020-10-02 01:40:12 +0530

    test error

--- plugins/sendgrid/core-plugin.php
+++ plugins/sendgrid/core-plugin.php
@@ -4,7 +4,9 @@ $sendgrid = new SendGrid();
 class SendGrid {
 
     function send_email ($mailr=array(), $show_errors=0) {
+        echo 'wwwww';
       if (isset($mailr['to_email']) && defined(ABSOLUTE_PATH) && defined(CONTACT_EMAIL) && defined(WEBSITE_NAME) && defined(SENDGRID_API_KEY)) {
+        echo 'vvvv';
         require_once ABSOLUTE_PATH.'/plugins/sendgrid/sendgrid-php.php';
         $email = new \SendGrid\Mail\Mail(); 
         $email->setFrom(CONTACT_EMAIL, WEBSITE_NAME);
