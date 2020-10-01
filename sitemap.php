f798b99ced124e3fd87b6f9cab5ad1b9b46dc578
akshay / wildfire <akshay.wildfire@pm.me>
2020-10-02 03:43:10 +0530

    removing white spaces in xml

--- sitemap.php
+++ sitemap.php
@@ -6,10 +6,8 @@ $types=$dash::get_types(THEME_PATH.'/config/types.json');
 header('Content-type: application/xml; charset=utf-8');
 $or=array();
 $xml = new SimpleXMLElement('<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"></urlset>');
-
 $or['url']=array('loc'=>BASE_URL, 'lastmod'=>'2020-06-20', 'priority'=>'1');
 to_xml($xml, $or);
-
 foreach ($types['webapp']['searchable_types'] as $tp) {
 	$posts=$sql->executeSQL("SELECT `id`, `content`->>'$.slug' `slug`, `updated_on` FROM `data` WHERE `content`->'$.content_privacy'='public' && `content`->'$.type'='$tp' ORDER BY `id` DESC");
 	foreach ($posts as $post) {
@@ -17,9 +15,7 @@ foreach ($types['webapp']['searchable_types'] as $tp) {
 		to_xml($xml, $or);
 	}
 }
-
 print $xml->asXML();
-
 function to_xml(SimpleXMLElement $object, array $data) {
     foreach ($data as $key => $value) {
         if (is_array($value)) {
