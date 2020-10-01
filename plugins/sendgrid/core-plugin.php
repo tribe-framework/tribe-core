0daa40f902152c1c03bddd10a70159c50a99c391
akshay / wildfire <akshay.wildfire@pm.me>
2020-10-02 01:43:22 +0530

    defined corrected

--- plugins/sendgrid/core-plugin.php
+++ plugins/sendgrid/core-plugin.php
@@ -4,9 +4,7 @@ $sendgrid = new SendGrid();
 class SendGrid {
 
     function send_email ($mailr=array(), $show_errors=0) {
-        echo 'wwwww'.WEBSITE_NAME.CONTACT_EMAIL.SENDGRID_API_KEY;
-      if (isset($mailr['to_email']) && defined(ABSOLUTE_PATH) && defined(CONTACT_EMAIL) && defined(WEBSITE_NAME) && defined(SENDGRID_API_KEY)) {
-        echo 'vvvv';
+      if (isset($mailr['to_email']) && defined('ABSOLUTE_PATH') && defined('CONTACT_EMAIL') && defined('WEBSITE_NAME') && defined('SENDGRID_API_KEY')) {
         require_once ABSOLUTE_PATH.'/plugins/sendgrid/sendgrid-php.php';
         $email = new \SendGrid\Mail\Mail(); 
         $email->setFrom(CONTACT_EMAIL, WEBSITE_NAME);
