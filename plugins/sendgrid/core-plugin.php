e6603a24d9456ddfc7ec639a9bb913cd01406441
akshay / wildfire <akshay.wildfire@pm.me>
2020-10-02 01:14:42 +0530

    sendgrid email sending

--- plugins/sendgrid/core-plugin.php
+++ plugins/sendgrid/core-plugin.php
@@ -1,35 +1,36 @@
 <?php
+$sendgrid = new SendGrid();
+class SendGrid {
+    function send_email ($mailr=array()) {
+      if ($mailr['to_email'] && ABSOLUTE_PATH && CONTACT_EMAIL && WEBSITE_NAME && SENDGRID_API_KEY) {
+        require_once ABSOLUTE_PATH.'/plugins/sendgrid/sendgrid-php.php';
+        $email = new \SendGrid\Mail\Mail(); 
+        $email->setFrom(CONTACT_EMAIL, WEBSITE_NAME);
+        $email->setSubject($mailr['subject']);
+        $email->addTo($mailr['to_email'], ($mailr['to_name']??''));
+        if (isset($mailr['body_text']))
+    	    $email->addContent("text/plain", $mailr['body_text']);
+        $email->addContent("text/html", $mailr['body_html']);
+        $sendgrid = new \SendGrid(SENDGRID_API_KEY);
+        $response = $sendgrid->send($email);
+      }
+    }
 
-function send_email ($mailr=array()) {
-  if ($mailr['to_email'] && ABSOLUTE_PATH && CONTACT_EMAIL && WEBSITE_NAME && SENDGRID_API_KEY) {
-    require_once ABSOLUTE_PATH.'/plugins/sendgrid/sendgrid-php.php';
-    $email = new \SendGrid\Mail\Mail(); 
-    $email->setFrom(CONTACT_EMAIL, WEBSITE_NAME);
-    $email->setSubject($mailr['subject']);
-    $email->addTo($mailr['to_email'], ($mailr['to_name']??''));
-    if (isset($mailr['body_text']))
-	    $email->addContent("text/plain", $mailr['body_text']);
-    $email->addContent("text/html", $mailr['body_html']);
-    $sendgrid = new \SendGrid(SENDGRID_API_KEY);
-    $response = $sendgrid->send($email);
-  }
+    function send_email_to_json_list ($filepath, $mailr=array()) {
+    	$emails=json_decode(file_get_contents($filepath), true);
+        $i=0;
+    	foreach ($emails as $email_row) {
+            if (!$i)
+                $fields=explode(',', '{'.implode('},{', array_keys($email_row)).'}');
+            $mail_arr=$mailr;
+    		$mail_arr['to_email']=$email_row['email'];
+            if ($email_row['link'])
+                $mail_arr['body_html']=str_replace($fields, $email_row, $mailr['body_html']);
+            else
+                $mail_arr['body_html']=$mailr['link_html'].$mailr['body_html'];
+    		send_email($mail_arr);
+            $i++;
+    	}
+    }
 }
-
-function send_email_to_json_list ($filepath, $mailr=array()) {
-	$emails=json_decode(file_get_contents($filepath), true);
-    $i=0;
-	foreach ($emails as $email_row) {
-        if (!$i)
-            $fields=explode(',', '{'.implode('},{', array_keys($email_row)).'}');
-        $mail_arr=$mailr;
-		$mail_arr['to_email']=$email_row['email'];
-        if ($email_row['link'])
-            $mail_arr['body_html']=str_replace($fields, $email_row, $mailr['body_html']);
-        else
-            $mail_arr['body_html']=$mailr['link_html'].$mailr['body_html'];
-		send_email($mail_arr);
-        $i++;
-	}
-}
-
 ?>
