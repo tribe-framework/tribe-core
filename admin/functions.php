44871857ad17584b61149148854dd567c23b6c46
akshay / wildfire <akshay.wildfire@pm.me>
2020-10-04 18:14:50 +0530

    print details

--- admin/functions.php
+++ admin/functions.php
@@ -90,7 +90,8 @@ function is_access_allowed ($id, $user_restricted_to_input_modules=array()) {
     if (count($user_restricted_to_input_modules)) {
     	echo 'entered ';
       foreach ($user_restricted_to_input_modules as $key => $value) {
-      	echo 'in_loop '.$id.' '.$value.' ';
+      	echo 'in_loop '.$id.' '.$value.' '.$dash->get_content_meta($id, $value).' '.$session_user[$value];
+
         if ($session_user[$value]==$dash->get_content_meta($id, $value)) {
         	echo 'here <br>';
           $allowed_access=1;
