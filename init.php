bec87c44571835a85090be270eaf5f38a94b6d61
Apurv Jyotirmay <apjyotirmay@gmail.com>
2020-11-06 12:38:27 +0530

    removed api from init

--- init.php
+++ init.php
@@ -47,27 +47,12 @@ isset($types['webapp']['lang'])?:$types['webapp']['lang']='en';
 if (isset($_GET['ext'])) { //for theme
     $ext=explode('/', $_GET['ext']);
 
-    if ($ext[0]=='api') {        
-        header('Content-Type: application/json');
-        if (is_numeric($ext[1]))
-            echo json_encode($dash->get_content($ext[1]));
-        else {
-            $type=$ext[1];
-            $typedata=$types[$type];
-            $typedata['post_ids']=$dash->get_all_ids($type);
-            echo json_encode($typedata);
-        }
-        die();
+    if (count($ext)) {
+        $type=$dash->do_unslugify($ext[0]);
     }
 
-    else {
-        if (count($ext)) {
-            $type=$dash->do_unslugify($ext[0]);
-        }
-
-        if (count($ext)>1) {
-            $slug=$dash->do_unslugify($ext[1]);
-        }
+    if (count($ext)>1) {
+        $slug=$dash->do_unslugify($ext[1]);
     }
 } elseif (isset($_GET['type'])) { //for dashboard
     $type=$dash->do_unslugify($_GET['type']);
