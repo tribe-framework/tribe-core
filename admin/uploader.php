59195fe758f3fec24f47a6ee5476d0c5fc19a0c4
akshay / wildfire <akshay.wildfire@pm.me>
2020-11-21 17:54:34 +0530

    max_width and max_height in blueimp

--- admin/uploader.php
+++ admin/uploader.php
@@ -5,7 +5,7 @@ include_once ('../init.php');
 //https://github.com/blueimp/jQuery-File-Upload/blob/master/server/php/UploadHandler.php
 include_once(ABSOLUTE_PATH.'/plugins/blueimp-jquery-file-upload/UploadHandler.php');
 
-if (UPLOAD_FILE_TYPES)
-	$upload_handler = new UploadHandler(array('script_url'=>ABSOLUTE_PATH.'/admin/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES));
+if (defined('UPLOAD_FILE_TYPES'))
+	$upload_handler = new UploadHandler(array('script_url'=>ABSOLUTE_PATH.'/admin/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES, 'max_height'=>(isset(UPLOAD_MAX_HEIGHT)?UPLOAD_MAX_HEIGHT:null), 'max_width'=>(isset(UPLOAD_MAX_WIDTH)?UPLOAD_MAX_WIDTH:null)));
 else
 	$upload_handler = new UploadHandler(array('script_url'=>ABSOLUTE_PATH.'/admin/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/'));
