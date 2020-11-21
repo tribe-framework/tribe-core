062f0d82c8004520bc8bf09ab7ea74ad98653166
akshay / wildfire <akshay.wildfire@pm.me>
2020-11-21 18:51:44 +0530

    max_width and max_height in blueimp

--- admin/uploader.php
+++ admin/uploader.php
@@ -6,6 +6,6 @@ include_once ('../init.php');
 include_once(ABSOLUTE_PATH.'/plugins/blueimp-jquery-file-upload/UploadHandler.php');
 
 if (defined('UPLOAD_FILE_TYPES'))
-	$upload_handler = new UploadHandler(array('script_url'=>ABSOLUTE_PATH.'/admin/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES, 'max_height'=>(isset(UPLOAD_MAX_HEIGHT)?UPLOAD_MAX_HEIGHT:null), 'max_width'=>(isset(UPLOAD_MAX_WIDTH)?UPLOAD_MAX_WIDTH:null)));
+	$upload_handler = new UploadHandler(array('script_url'=>ABSOLUTE_PATH.'/admin/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'inline_file_types'=>UPLOAD_FILE_TYPES, 'accept_file_types'=>UPLOAD_FILE_TYPES, 'max_height'=>(defined(UPLOAD_MAX_HEIGHT)?UPLOAD_MAX_HEIGHT:null), 'max_width'=>(defined(UPLOAD_MAX_WIDTH)?UPLOAD_MAX_WIDTH:null)));
 else
-	$upload_handler = new UploadHandler(array('script_url'=>ABSOLUTE_PATH.'/admin/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/'));
+	$upload_handler = new UploadHandler(array('script_url'=>ABSOLUTE_PATH.'/admin/uploader.php', 'upload_dir'=>ABSOLUTE_PATH.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'upload_url'=>BASE_URL.'/uploads/'.date('Y').'/'.date('m-F').'/'.date('d-D').'/', 'max_height'=>(defined(UPLOAD_MAX_HEIGHT)?UPLOAD_MAX_HEIGHT:null), 'max_width'=>(defined(UPLOAD_MAX_WIDTH)?UPLOAD_MAX_WIDTH:null)));
