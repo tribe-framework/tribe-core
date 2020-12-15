6ff162053bae4496297c132263e9320c8ecfd982
akshay / jigmet <akshay.wildfire@pm.me>
2020-12-16 01:41:20 +0530

    using dash variables in theme

--- src/Core/Theme.php
+++ src/Core/Theme.php
@@ -10,35 +10,35 @@ is_ is for a yes/no answer
 
 namespace Wildfire\Core;
 
-class Theme
-{
+class Theme {
 
-    public static $last_error = null; //array of error messages
-    public static $last_info = null; //array of info messages
-    public static $last_data = null; //array of data to be sent for display
-    public static $last_redirect = null; //redirection url
+	public static $last_error = null; //array of error messages
+	public static $last_info = null; //array of info messages
+	public static $last_data = null; //array of data to be sent for display
+	public static $last_redirect = null; //redirection url
 
-    public function __construct()
-    {
+	public function __construct() {
+		$this->dash = new Wildfire\Core\Dash();
+	}
 
-    }
+	public function get_navbar_menu($slug = '', $css_classes = array('navbar' => 'navbar-expand-md navbar-light bg-light', 'ul' => 'navbar-nav ml-auto mr-0', 'li' => 'nav-item', 'a' => 'nav-link', 'toggler' => 'navbar-toggler'), $hamburger_bars = '<span class="navbar-toggler-icon"></span>') {
 
-    public function get_navbar_menu($slug = '', $css_classes = array('navbar' => 'navbar-expand-md navbar-light bg-light', 'ul' => 'navbar-nav ml-auto mr-0', 'li' => 'nav-item', 'a' => 'nav-link', 'toggler' => 'navbar-toggler'), $hamburger_bars = '<span class="navbar-toggler-icon"></span>')
-    {
-        global $menus, $types, $dash, $_SESSION, $userless_install;
+		$types = $this->dash->getTypes();
+		$menus = $this->dash->getMenus();
+		$session_user = $this->dash->getSessionUser();
 
-        if (is_array($slug)) {
-            $items = $slug;
-        } else if ($slug) {
-            $items = $menus[$slug];
-        } else {
-            $items = 0;
-        }
+		if (is_array($slug)) {
+			$items = $slug;
+		} else if ($slug) {
+			$items = $menus[$slug];
+		} else {
+			$items = 0;
+		}
 
-        $op = '';
+		$op = '';
 
-        if ($items) {
-            $op .= '
+		if ($items) {
+			$op .= '
 			<nav class="navbar ' . $css_classes['navbar'] . '">
 				<a class="navbar-brand" href="' . ($items['logo']['href'] ?? '#') . '" title="' . ($items['logo']['title'] ?? BASE_URL . ' logo') . '">' . (isset($items['logo']['src']) && trim($items['logo']['src']) ? '<img ' . (isset($items['logo']['height']) ? 'height="' . $items['logo']['height'] . '"' : '') . ' src="' . $items['logo']['src'] . '">' : (isset($items['logo']['name']) ? $items['logo']['name'] . (isset($items['logo']['byline']) ? '<span class="small byline">' . $items['logo']['byline'] . '</span>' : '') : 'Wildfire')) . '</a>
 				<button class="' . ($css_classes['toggler'] ?? '') . '" type="button" data-toggle="collapse" data-target="#navbarSupportedContent">
@@ -46,66 +46,66 @@ class Theme
 				</button>
 				<div class="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul class="' . $css_classes['ul'] . '">';
-            if (isset($items['menu'])) {
-                foreach ($items['menu'] as $item) {
-                    if ($item['admin_access_only'] && !$userless_install && $types['user']['roles'][$_SESSION[user][role_slug]]['role'] != 'admin') {
-                        continue;
-                    }
+			if (isset($items['menu'])) {
+				foreach ($items['menu'] as $item) {
+					if ($item['admin_access_only'] && $types['user']['roles'][$session_user[role_slug]]['role'] != 'admin') {
+						continue;
+					}
 
-                    if (is_array(($item['submenu'] ?? ''))) {
-                        $op .= '<li class="' . ($css_classes['li'] ?? '') . ' dropdown"><a class="' . ($css_classes['a'] ?? '') . ' dropdown-toggle" href="#" title="' . ($item['title'] ?? '') . '" role="button" data-toggle="dropdown">' . ($item['name'] ?? '') . '
+					if (is_array(($item['submenu'] ?? ''))) {
+						$op .= '<li class="' . ($css_classes['li'] ?? '') . ' dropdown"><a class="' . ($css_classes['a'] ?? '') . ' dropdown-toggle" href="#" title="' . ($item['title'] ?? '') . '" role="button" data-toggle="dropdown">' . ($item['name'] ?? '') . '
 									</a><div class="dropdown-menu ' . ($css_classes['dropdown'] ?? '') . ' ' . ($item['dropdown_class'] ?? '') . '">';
-                        if (isset($item['submenu'])) {
-                            foreach ($item['submenu'] as $subitem) {
-                                $op .= '<a class="dropdown-item" href="' . ($subitem['href'] ?? '') . '" title="' . ($subitem['title'] ?? '') . '">' . ($subitem['name'] ?? '') . '</a>';
-                            }
+						if (isset($item['submenu'])) {
+							foreach ($item['submenu'] as $subitem) {
+								$op .= '<a class="dropdown-item" href="' . ($subitem['href'] ?? '') . '" title="' . ($subitem['title'] ?? '') . '">' . ($subitem['name'] ?? '') . '</a>';
+							}
 
-                        }
-                        $op .= '</div></li>';
-                    } else if (isset($item['submenu'])) {
-                        $submenu = $item['submenu'];
-                        $is_user_role_menu = 0;
-                        if (is_array(($types[$submenu]['roles'] ?? ''))) {
-                            $subitems = $types[$submenu]['roles'];
-                            $is_user_role_menu = 1;
-                        } else {
-                            $subitems = $dash::get_all_ids(($item['submenu'] ?? ''), ($types[$submenu]['priority_field'] ?? ''), ($types[$submenu]['priority_order'] ?? ''));
-                        }
+						}
+						$op .= '</div></li>';
+					} else if (isset($item['submenu'])) {
+						$submenu = $item['submenu'];
+						$is_user_role_menu = 0;
+						if (is_array(($types[$submenu]['roles'] ?? ''))) {
+							$subitems = $types[$submenu]['roles'];
+							$is_user_role_menu = 1;
+						} else {
+							$subitems = $this->dash::get_all_ids(($item['submenu'] ?? ''), ($types[$submenu]['priority_field'] ?? ''), ($types[$submenu]['priority_order'] ?? ''));
+						}
 
-                        $op .= '<li class="' . ($css_classes['li'] ?? '') . ' dropdown"><a class="' . ($css_classes['a'] ?? '') . ' dropdown-toggle" href="#" title="' . ($item['title'] ?? '') . '" role="button" data-toggle="dropdown">' . ($item['name'] ?? '') . '
+						$op .= '<li class="' . ($css_classes['li'] ?? '') . ' dropdown"><a class="' . ($css_classes['a'] ?? '') . ' dropdown-toggle" href="#" title="' . ($item['title'] ?? '') . '" role="button" data-toggle="dropdown">' . ($item['name'] ?? '') . '
 									</a><div class="dropdown-menu ' . ($css_classes['dropdown'] ?? '') . ' ' . ($item['dropdown_class'] ?? '') . '">';
-                        foreach ($subitems as $key => $opt) {
-                            if ($is_user_role_menu) {
-                                $subitem = $opt;
-                                $subitem['href'] = '/admin/list?type=' . $item['submenu'] . '&role=' . $key;
-                            } else {
-                                $subitem = $dash::get_content($opt['id']);
-                                $subitem['href'] = '/' . $item['submenu'] . '/' . $subitem['slug'];
-                            }
-                            $op .= '<a class="dropdown-item" href="' . ($subitem['href'] ?? '') . '">' . ($subitem['title'] ?? '') . '</a>';
-                        }
-                        $op .= '</div></li>';
-                    } else {
-                        $data_ext = '';
-                        if (isset($item['data'])) {
-                            foreach ($item['data'] as $data) {
-                                foreach ($data as $k => $v) {
-                                    $data_ext .= 'data-' . $k . '="' . $v . '" ';
-                                }
-                            }
-                        }
+						foreach ($subitems as $key => $opt) {
+							if ($is_user_role_menu) {
+								$subitem = $opt;
+								$subitem['href'] = '/admin/list?type=' . $item['submenu'] . '&role=' . $key;
+							} else {
+								$subitem = $this->dash::get_content($opt['id']);
+								$subitem['href'] = '/' . $item['submenu'] . '/' . $subitem['slug'];
+							}
+							$op .= '<a class="dropdown-item" href="' . ($subitem['href'] ?? '') . '">' . ($subitem['title'] ?? '') . '</a>';
+						}
+						$op .= '</div></li>';
+					} else {
+						$data_ext = '';
+						if (isset($item['data'])) {
+							foreach ($item['data'] as $data) {
+								foreach ($data as $k => $v) {
+									$data_ext .= 'data-' . $k . '="' . $v . '" ';
+								}
+							}
+						}
 
-                        $op .= '<li class="' . ($css_classes['li'] ?? '') . '"><a class="' . ($css_classes['a'] ?? '') . '" ' . ($data_ext ?? '') . ' href="' . ($item['href'] ?? '') . '" title="' . ($item['title'] ?? '') . '">' . ($item['name'] ?? '') . '</a></li>';
-                    }
+						$op .= '<li class="' . ($css_classes['li'] ?? '') . '"><a class="' . ($css_classes['a'] ?? '') . '" ' . ($data_ext ?? '') . ' href="' . ($item['href'] ?? '') . '" title="' . ($item['title'] ?? '') . '">' . ($item['name'] ?? '') . '</a></li>';
+					}
 
-                }
-            }
-            $op .= '
+				}
+			}
+			$op .= '
 					</ul>
 				</div>
 			</nav>';
-        } else {
-            $op .= '
+		} else {
+			$op .= '
 			<nav class="navbar ' . ($css_classes['navbar'] ?? '') . '">
 				<a class="navbar-brand" href="/">Website</a>
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -142,74 +142,76 @@ class Theme
 					</form>
 				</div>
 			</nav>';
-        }
+		}
 
-        return $op;
-    }
+		return $op;
+	}
 
-    public function get_menu($slug = '', $css_classes = array('ul' => 'justify-content-center', 'li' => 'nav-item', 'a' => 'nav-link'))
-    {
-        global $menus, $types, $dash, $_SESSION, $userless_install;
+	public function get_menu($slug = '', $css_classes = array('ul' => 'justify-content-center', 'li' => 'nav-item', 'a' => 'nav-link')) {
 
-        if (is_array($slug)) {
-            $items = $slug;
-        } else if ($slug) {
-            $items = $menus[$slug];
-        } else {
-            $items = 0;
-        }
+		$types = $this->dash->getTypes();
+		$menus = $this->dash->getMenus();
+		$session_user = $this->dash->getSessionUser();
 
-        $op = '';
+		if (is_array($slug)) {
+			$items = $slug;
+		} else if ($slug) {
+			$items = $menus[$slug];
+		} else {
+			$items = 0;
+		}
 
-        if ($items) {
-            $op .= '
+		$op = '';
+
+		if ($items) {
+			$op .= '
 			<ul class="' . ($css_classes['ul'] ?? '') . '">';
-            if (isset($items['menu'])) {
-                foreach ($items['menu'] as $item) {
-                    if ($item['admin_access_only'] && !$userless_install && $types['user']['roles'][$_SESSION[user][role_slug]]['role'] != 'admin') {
-                        continue;
-                    }
+			if (isset($items['menu'])) {
+				foreach ($items['menu'] as $item) {
+					if ($item['admin_access_only'] && $types['user']['roles'][$session_user[role_slug]]['role'] != 'admin') {
+						continue;
+					}
 
-                    if (is_array(($item['submenu'] ?? ''))) {
-                        $op .= '<li class="dropdown ' . ($css_classes['li'] ?? '') . '"><a class="' . ($css_classes['a'] ?? '') . ' dropdown-toggle" href="#" title="' . ($item['title'] ?? '') . '" role="button" data-toggle="dropdown">' . ($item['name'] ?? '') . '
+					if (is_array(($item['submenu'] ?? ''))) {
+						$op .= '<li class="dropdown ' . ($css_classes['li'] ?? '') . '"><a class="' . ($css_classes['a'] ?? '') . ' dropdown-toggle" href="#" title="' . ($item['title'] ?? '') . '" role="button" data-toggle="dropdown">' . ($item['name'] ?? '') . '
 								</a><div class="dropdown-menu ' . ($css_classes['dropdown'] ?? '') . ' ' . ($item['dropdown_class'] ?? '') . '">';
-                        if (isset($item['submenu'])) {
-                            foreach ($item['submenu'] as $subitem) {
-                                $op .= '<a class="dropdown-item" href="' . ($subitem['href'] ?? '') . '" title="' . ($subitem['title'] ?? '') . '">' . ($subitem['name'] ?? '') . '</a>';
-                            }
+						if (isset($item['submenu'])) {
+							foreach ($item['submenu'] as $subitem) {
+								$op .= '<a class="dropdown-item" href="' . ($subitem['href'] ?? '') . '" title="' . ($subitem['title'] ?? '') . '">' . ($subitem['name'] ?? '') . '</a>';
+							}
 
-                        }
-                        $op .= '</div></li>';
-                    } else if (isset($item['submenu'])) {
-                        $submenu = $item['submenu'];
-                        $subitems = $dash::get_all_ids($item['submenu'], ($types[$submenu]['priority_field'] ?? ''), ($types[$submenu]['priority_order'] ?? ''));
-                        $op .= '<li class="dropdown ' . ($css_classes['li'] ?? '') . '"><a class="' . ($css_classes['a'] ?? '') . ' dropdown-toggle" href="#" title="' . ($item['title'] ?? '') . '" role="button" data-toggle="dropdown">' . ($item['name'] ?? '') . '
+						}
+						$op .= '</div></li>';
+					} else if (isset($item['submenu'])) {
+						$submenu = $item['submenu'];
+						$subitems = $this->dash::get_all_ids($item['submenu'], ($types[$submenu]['priority_field'] ?? ''), ($types[$submenu]['priority_order'] ?? ''));
+						$op .= '<li class="dropdown ' . ($css_classes['li'] ?? '') . '"><a class="' . ($css_classes['a'] ?? '') . ' dropdown-toggle" href="#" title="' . ($item['title'] ?? '') . '" role="button" data-toggle="dropdown">' . ($item['name'] ?? '') . '
 								</a><div class="dropdown-menu ' . ($css_classes['dropdown'] ?? '') . ' ' . ($item['dropdown_class'] ?? '') . '">';
-                        foreach ($subitems as $opt) {
-                            $subitem = $dash::get_content($opt['id']);
-                            $op .= '<a class="dropdown-item" href="/' . $item['submenu'] . '/' . $subitem['slug'] . '">' . ($subitem['title'] ?? '') . '</a>';
-                        }
-                        $op .= '</div></li>';
-                    } else {
-                        $data_ext = '';
-                        if (isset($item['data'])) {
-                            foreach ($item['data'] as $data) {
-                                foreach ($data as $k => $v) {
-                                    $data_ext .= 'data-' . $k . '="' . $v . '" ';
-                                }
+						foreach ($subitems as $opt) {
+							$subitem = $this->dash::get_content($opt['id']);
+							$op .= '<a class="dropdown-item" href="/' . $item['submenu'] . '/' . $subitem['slug'] . '">' . ($subitem['title'] ?? '') . '</a>';
+						}
+						$op .= '</div></li>';
+					} else {
+						$data_ext = '';
+						if (isset($item['data'])) {
+							foreach ($item['data'] as $data) {
+								foreach ($data as $k => $v) {
+									$data_ext .= 'data-' . $k . '="' . $v . '" ';
+								}
 
-                            }
-                        }
+							}
+						}
 
-                        $op .= '<li class="' . ($css_classes['li'] ?? '') . '"><a class="' . ($css_classes['a'] ?? '') . '" ' . ($data_ext) . ' href="' . ($item['href'] ?? '') . '" title="' . ($item['title'] ?? '') . '">' . ($item['name'] ?? '') . '</a></li>';
-                    }
+						$op .= '<li class="' . ($css_classes['li'] ?? '') . '"><a class="' . ($css_classes['a'] ?? '') . '" ' . ($data_ext) . ' href="' . ($item['href'] ?? '') . '" title="' . ($item['title'] ?? '') . '">' . ($item['name'] ?? '') . '</a></li>';
+					}
 
-                }
-            }
-            $op .= '
+				}
+			}
+			$op .= '
 				</ul>';
-        } else {
-            $op .= '
+		} else {
+			$op .= '
 					<ul class="' . ($css_classes['ul'] ?? '') . '">
 					  <li class="' . ($css_classes['li'] ?? '') . '">
 					    <a class="' . ($css_classes['a'] ?? '') . ' active" href="#">Active</a>
@@ -224,8 +226,8 @@ class Theme
 					    <a class="' . ($css_classes['a'] ?? '') . ' disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
 					  </li>
 					</ul>';
-        }
+		}
 
-        return $op;
-    }
+		return $op;
+	}
 }
