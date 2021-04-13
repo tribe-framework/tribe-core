b4e769c7b425e2de11c91cdc3a1e9f06e1f3d009
akshay / wildfire <akshay.wildfire@pm.me>
2021-04-13 13:41:20 +0530

    Half files do not need URL.
    Revert "includes naming for half files"
    
    This reverts commit 828537cdb3e8171c67765cb444207d30c4124cbc.

--- src/Init.php
+++ src/Init.php
@@ -349,33 +349,6 @@ class Init {
 			return true;
 		}
 
-		// checking for "/theme/includes/$type/_$slug.php"
-		$file_path = THEME_PATH . '/includes/' . $type . '/_' . $slug . '.php';
-
-		if (file_exists($file_path)) {
-			include_once $file_path;
-			unset($file_path);
-			return true;
-		}
-
-		// checking for "/theme/includes/_$type-$slug.php"
-		$file_path = THEME_PATH . '/includes/_' . $type . '-' . $slug . '.php';
-
-		if (file_exists($file_path)) {
-			include_once $file_path;
-			unset($file_path);
-			return true;
-		}
-
-		// checking for "/theme/includes/_single-ID.php"
-		$file_path = THEME_PATH . '/includes/_single-' . $postdata['id'] . '.php';
-
-		if (file_exists($file_path)) {
-			include_once $file_path;
-			unset($file_path);
-			return true;
-		}
-
 		// checking for "/theme/pages/single-$type.php"
 		$file_path = THEME_PATH . '/pages/single-' . $type . '.php';
 
