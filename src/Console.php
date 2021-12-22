b34a9d3b18e226aa8734f002c6be6a807beb625b
Apurv Jyotirmay <mail@jyotirmay.dev>
2021-12-22 16:40:40 +0530

    json logging capability

--- src/Console.php
+++ src/Console.php
@@ -3,9 +3,10 @@
 namespace Wildfire\Core;
 
 class Console {
-    public static function debug($data, bool $halt = false) {
+    public static function debug($data, bool $halt = false)
+    {
         echo '<div
-            class="container"
+            class="container my-2"
             style="
                 background-color:black;
                 padding: 0.5rem 1rem;
@@ -13,7 +14,7 @@ class Console {
                 box-shadow: 0 0 0 4px #000, inset 0 0 0 2px purple;
             "
             ><pre
-                class="small col-md-10 mx-auto"
+                class="small col-md-10 mx-auto text-white"
                 style="
                     white-space:pre-wrap;
                     color:#fff;
@@ -25,5 +26,26 @@ class Console {
         if ($halt) {
             die();
         }
+    }
+
+    public static function json($data, bool $halt = false)
+    {
+        echo '<div
+            class="container my-2"
+            style="
+                background-color:black;
+                padding: 0.5rem 1rem;
+                border-radius: 8px;
+                box-shadow: 0 0 0 4px #000, inset 0 0 0 2px purple;
+            "
+            ><pre
+                class="small col-md-10 mx-auto text-white"
+                style="
+                    white-space:pre-wrap;
+                    color:#fff;
+                "
+                >'.json_encode($data, JSON_PRETTY_PRINT).
+            '</pre>
+        </div>';
     }
 }
